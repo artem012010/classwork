cb736e2d2cbcac8614de21e8b4c136f6fbe38f6d
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-09-17 13:01:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -61,3 +61,16 @@ if searching in trips:
 else:
    print('Такого пожелания нет')
 
+
+subjects = list()
+subject = input('Введи предмет (0 - остановить ввод):')
+subject = subject.lower()
+while subject != '0':
+   if subject in subjects:
+       print('Этот предмет уже записан')
+   else:
+       subjects.append(subject)
+   subject = input('Введи предмет (0 - остановить ввод):')
+   subject = subject.lower()
+subjects.sort()
+print('Список предметов:', subjects)
