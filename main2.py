3ed9d15513dfa7e231781abe298ca40442920585
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-09-17 14:06:17 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -7,3 +7,17 @@ while balance > 0 and input('Хотите сделать покупку (да/н
         balance -= price
         print('Сумма', price, 'руб. списана со счёта.')
 print('Доступная сумма:', balance, 'руб.')
+
+
+months = 'январь февраль март апрель май июнь июль август сентябрь октябрь ноябрь декабрь'.split()
+sales = input('Введите данные о доходах с января по декабрь').split()
+sales = list(map(int, sales))
+growth = []
+fall = []
+for i in range(1, len(sales)):
+    if sales[i] > sales[i-1]:
+        growth.append(months[i])
+    if sales[i] < sales[i-1]:
+        fall.append(months[i])
+print('Месяцы роста доходов:', growth)
+print('Месяцы снижения доходов:', fall)
