1a00b96d2bf967a456bf59aef3896e6b14c20ea5
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-09-21 17:23:00 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,12 @@
-while True:
-   try:
-       children_amount = int(input('Введите число детей'))
-       sweets_amount = int(input('Введите количество конфет'))
-       break
-   except:
-       print('Ошибка! Количество должно быть целым числом')
-try:
-   portion = sweets_amount/children_amount
-   print('Каждый ребёнок получит', portion, 'конфет(-ы)')
-except:
-   print('Ошибка деления! Возможно, пришло 0 детей?')
-
-
-print('Как называется самая высокая гора мира?')
-while True:
-    answer = input('1 - Эльбрус, 2 - Мауна-Лоа, 3 - Эверест, 4 - Денали')
-    try:
-        answer = int(answer)
-        break
-    except:
-        print('Ошибка! Введите номер правильного ответа')
-if answer == 3:
-    print('Абсолютно верно!')
-else:
-    print('Нет. Эверест, 8848 метров')
-
-
-print('Как расшифровывается ВОЗ?')
-print('1-Всемирная организация здравоохранения')
-print('2-Всероссийская организация защиты')
-print('3-Всероссийская особая здравница')
-print('4-Владение особого значения')
-while True:
-   try:
-       answer = int(input())
-       break
-   except:
-       print('Ошибка! Введите номер правильного ответа')
-if answer == 1:
-   print('Абсолютно верно!')
-else:
-   print('Нет. ВОЗ - это Всемирная организация здравоохранения')
-
-
-students = ['Смирнов', 'Иванов', 'Васильев', 'Молоткова', 'Алексашкина', 'Гришин']
-amount_students = len(students)
-students.sort()
-i = 1
-print('Список класса:')
-for student in students:
-   print(i, '-', student)
-   i += 1
-print('Всего учеников:', amount_students)
-
-trips = ['эрмитаж', 'московский зоопарк', 'кремль', 'зоологический музей']
-searching = input('Запрос:')
-searching = searching.lower()
-if searching in trips:
-   print('Запрос найден в пожеланиях')
-else:
-   print('Такого пожелания нет')
-
-
-subjects = list()
-subject = input('Введи предмет (0 - остановить ввод):')
-subject = subject.lower()
-while subject != '0':
-   if subject in subjects:
-       print('Этот предмет уже записан')
-   else:
-       subjects.append(subject)
-   subject = input('Введи предмет (0 - остановить ввод):')
-   subject = subject.lower()
-subjects.sort()
-print('Список предметов:', subjects)
+months = 'январь февраль март апрель май июнь июль август сентябрь октябрь ноябрь декабрь'.split()
+sales = input('Введите данные о доходах с января по декабрь').split()
+sales = list(map(int, sales))
+growth = []
+fall = []
+for i in range(1, len(sales)):
+    if sales[i] > sales[i-1]:
+        growth.append(months[i])
+    if sales[i] < sales[i-1]:
+        fall.append(months[i])
+print('Месяцы роста доходов:', growth)
+print('Месяцы снижения доходов:', fall)
