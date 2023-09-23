2e9204e05488b933a8a9140ee934c50afed23ff9
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-09-23 12:10:52 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -9,3 +9,15 @@ if growth == 1:
    print('Стабильная успеваемость!')
 else:
    print('Стабильной успеваемости нет')
+
+
+
+
+marks = input('Введите оценки через пробел:')
+marks = marks.split(' ')
+amount_five = 0
+for mark in marks:
+  if mark == '5':
+      amount_five += 1
+chance = amount_five/len(marks)*100
+print('Коэффициент идеальности (%) -', chance)
