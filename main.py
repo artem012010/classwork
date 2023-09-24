d676cd1b84c49e00fc2dd7d45a5dd71d53ac5d5a
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-09-24 15:15:54 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,19 @@
-class Converter():
-   def __init__(self, usd):
-       self.usd = usd
- 
-   def rub_to_usd(self, rub_value):
-       usd_value = rub_value/self.usd
-       print(round(usd_value, 2))
- 
-   def usd_to_rub(self, usd_value):
-       rub_value = usd_value*self.usd
-       print(round(rub_value, 2))
- 
-a = int(input('Введите курс доллара'))
-b = int(input('Введите сумму для обмена'))
-conv = Converter(a)
-option = int(input('1 - доллары в рубли / 2 - рубли в доллары'))
-if option == 1:
-   conv.usd_to_rub(b)
-else:
-   conv.rub_to_usd(b)
+books = {
+  'Кинг': 'Оно',
+  'Лондон': 'Белый клык',
+  'Кэрролл': 'Алиса в стране чудес',
+  'Линдгрен': 'Карлсон, который живёт на крыше'}
+
+
+books['Дефо'] = 'Приключения Робинзона Крузо'
+books['Дюма'] = 'Граф Монте-Кристо'
+del books['Кинг']
+
+
+if 'Дефо' in books and 'Дюма' in books:
+   print('База обновлена!')
+if ('Кинг' in books) == False:
+   print('Предпочтения обновлены')
 
 
 
-
-class Accounting():
-    def __init__(self, account):
-        self.account = account
-    def pay_bill(self, amount):
-        if self.account - amount >= 0:
-            self.account -= amount
-            print(f'Списано {amount} руб. Остаток на счету: {self.account} руб.')
-        else:
-            print('Недостаточно средств для выполнения операции.')
-    def get_money(self, amount):
-        self.account += amount
-        print(f'Зачислено {amount} руб. Остаток на счету: {self.account} руб.')
-
-amount = int(input('Введите данные о сумме на счету'))
-office = Accounting(amount)
-while input('Хотите выполнить операцию? (1 - да, 2 - нет)') == '1':
-    code = input('Введите код операции (1 - зачисление, 2 - списание)')
-    amount = int(input('Введите сумму'))
-    if code == '1':
-        office.get_money(amount)
-    else:
-        office.pay_bill(amount)
-print('Спасибо за работу! Хорошего вечера!')
