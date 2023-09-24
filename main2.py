66f9b015b87c478589cf0e6b1e98256b9eed7a5d
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-09-24 17:33:15 +0600

    Update main2.py

--- main2.py
+++ main2.py
@@ -37,3 +37,16 @@ while input('Хотите сделать заказ? (1 - да, 0 - нет)') !=
 
 
 print('Спасибо за сотрудничество! Хорошего дня!')
+
+
+
+
+def count_services(**kwargs):
+    total = 0
+    categories = 0
+    for category in kwargs:
+        if kwargs[category] > 0:
+            total += kwargs[category]
+            categories += 1
+    return total, categories
+
