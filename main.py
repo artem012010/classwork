f8e7703e3f9784a4cd6cc1bef9bccd96eb08aa90
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-09-26 14:49:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,3 +18,29 @@ if option == 1:
    conv.usd_to_rub(b)
 else:
    conv.rub_to_usd(b)
+
+
+
+class Accounting():
+    def __init__(self, account):
+        self.account = account
+    def pay_bill(self, amount):
+        if self.account - amount >= 0:
+            self.account -= amount
+            print(f'Списано {amount} руб. Остаток на счету: {self.account} руб.')
+        else:
+            print('Недостаточно средств для выполнения операции.')
+    def get_money(self, amount):
+        self.account += amount
+        print(f'Зачислено {amount} руб. Остаток на счету: {self.account} руб.')
+
+amount = int(input('Введите данные о сумме на счету'))
+office = Accounting(amount)
+while input('Хотите выполнить операцию? (1 - да, 2 - нет)') == '1':
+    code = input('Введите код операции (1 - зачисление, 2 - списание)')
+    amount = int(input('Введите сумму'))
+    if code == '1':
+        office.get_money(amount)
+    else:
+        office.pay_bill(amount)
+print('Спасибо за работу! Хорошего вечера!')
