ea59cac8cc36fef9545632a8264c86c8be9e593b
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-09-26 14:46:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,108 +1,20 @@
-books = {
-  'Кинг': 'Оно',
-  'Лондон': 'Белый клык',
-  'Кэрролл': 'Алиса в стране чудес',
-  'Линдгрен': 'Карлсон, который живёт на крыше'}
-
-
-books['Дефо'] = 'Приключения Робинзона Крузо'
-books['Дюма'] = 'Граф Монте-Кристо'
-del books['Кинг']
-
-
-if 'Дефо' in books and 'Дюма' in books:
-   print('База обновлена!')
-if ('Кинг' in books) == False:
-   print('Предпочтения обновлены')
-
-
-
-
-
-authors = {
-   'Пушкин' : 'Русский поэт, драматург и прозаик. Один из самых авторитетных литературных деятелей первой трети XIX века',
-   'Толстой' : 'Один из наиболее известных русских писателей и мыслителей, один из величайших писателей-романистов мира',
-   'Бунин' : 'Русский писатель, поэт и переводчик, лауреат Нобелевской премии по литературе 1933 года'}
-surname = input('Фамилия автора:')
-if surname in authors:
-   print('Биография:', authors[surname])
+class Converter():
+   def __init__(self, usd):
+       self.usd = usd
+ 
+   def rub_to_usd(self, rub_value):
+       usd_value = rub_value/self.usd
+       print(round(usd_value, 2))
+ 
+   def usd_to_rub(self, usd_value):
+       rub_value = usd_value*self.usd
+       print(round(rub_value, 2))
+ 
+a = int(input('Введите курс доллара')) 
+b = int(input('Введите сумму для обмена')) 
+conv = Converter(a)
+option = int(input('1 - доллары в рубли / 2 - рубли в доллары'))
+if option == 1:
+   conv.usd_to_rub(b)
 else:
-   answer = input('Автор не найден. Добавить?')
-   answer = answer.lower()
-   if answer == 'да':
-       biography = input('Его биография:')
-       authors[surname] = biography
-       print(authors)
-   else:
-       print('Ответ получен')
-
-
-
-
-
-student_card = {'номер': '324', 'фамилия': 'Иванов'}
-print('Добро пожаловать!', student_card)
-answer = int(input('Личный кабинет: 1 - взять, 2 - вернуть, 3 - домой'))
-while answer != 3:
-   if answer == 1:
-       title = input('Введите название:')
-       student_card['долг'] = title
-       print('Карточка читателя:', student_card)
-   if answer == 2:
-       if 'долг' in student_card:
-           del student_card['долг']
-       print('Карточка читателя:', student_card)
-   answer = int(input('Ваше действие: 1 - взять, 2 - вернуть, 3 - домой'))
-print('Ждём вас:', student_card)
-
-
-my_shelf = dict()
-author = input('Введите автора:')
-book = input('Введите книгу (s - стоп):')
-books = list()
-while book != 's':
-   books.append(book)
-   book = input('Введите книгу (s - стоп):')
-my_shelf[author] = books
-print(my_shelf)
-
-
-my_shelf = dict()
-author = input('Введите автора (q - завершить):')
-while author != 'q':
-   books = list()
-   book = input('Введите книгу (s - стоп):')
-   while book != 's':
-       books.append(book)
-       book = input('Введите книгу (s - стоп):')
-   my_shelf[author] = books
-   author = input('Введите автора (q - завершить):')
-
-
-for author in my_shelf:
-   print(author, '-', my_shelf[author])
-
-
-
-
-from random import randint
-authors = {
-   'Пушкин': ['Сказки', 'Дубровский', 'Руслан и Людмила'],
-   'Бианки': ['Мышонок Пик', 'Лесная газета', 'Чей нос лучше'],
-   'Зощенко': ['Рассказы', 'Голубая книга', 'Лёля и Минька'],
-   'Лондон': ['Белый клык', 'Зов предков', 'Мартин Иден'],
-   'Заболоцкий': ['Стихи', 'Некрасивая девочка', 'Как мыши с котом воевали'],
-   'Кристи': ['Десять негритят', 'Убийство в Восточном экспрессе', 'Труп в библиотеке'],
-   'Распутин': ['Уроки французского', 'Дочь Ивана, мать Ивана', 'Рудольфио'],
-   'Тургенев': ['Бежин луг', 'Хорь и Калиныч', 'Первая любовь'],
-   'Толстой': ['Кавказский пленник', 'Детство', 'Филипок'],
-   'Астафьев': ['Конь с розовой гривой', 'Царь-рыба', 'Людочка']
-}
-
-
-author = input('Испытайте удачу! Введите автора:')
-if author in authors:
-   number = randint(0, 2)
-   print('Рекомендованная книга:', authors[author][number])
-else:
-   print('Автор не найден!')
+   conv.rub_to_usd(b)
