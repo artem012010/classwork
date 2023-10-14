f8908c47642820ce0da9782cc5dd7214b3c17cdc
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-10-14 15:37:08 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel)
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, 
+        QGroupBox, QRadioButton,  
+        QPushButton, QLabel)
 
 
 app = QApplication([])
 
 
-window = QWidget()
-window.setWindowTitle('Memo Card')
+# Создаем панель вопроса
+btn_OK = QPushButton('Ответить')
+lb_Question = QLabel('Самый сложный вопрос в мире!')
 
 
-'''Интерфейс приложения Memory Card'''
-btn_OK = QPushButton('Ответить') # кнопка ответа
-lb_Question = QLabel('В каком году была основана Москва?') # текст вопроса
+RadioGroupBox = QGroupBox("Варианты ответов")
 
 
-RadioGroupBox = QGroupBox("Варианты ответов") # группа на экране для переключателей с ответами
-rbtn_1 = QRadioButton('1147')
-rbtn_2 = QRadioButton('1242')
-rbtn_3 = QRadioButton('1861')
-rbtn_4 = QRadioButton('1943')
+rbtn_1 = QRadioButton('Вариант 1')
+rbtn_2 = QRadioButton('Вариант 2')
+rbtn_3 = QRadioButton('Вариант 3')
+rbtn_4 = QRadioButton('Вариант 4')
 
 
 layout_ans1 = QHBoxLayout()   
-layout_ans2 = QVBoxLayout() # вертикальные будут внутри горизонтального
+layout_ans2 = QVBoxLayout()
 layout_ans3 = QVBoxLayout()
 layout_ans2.addWidget(rbtn_1) # два ответа в первый столбец
 layout_ans2.addWidget(rbtn_2)
@@ -31,19 +33,35 @@ layout_ans3.addWidget(rbtn_4)
 
 
 layout_ans1.addLayout(layout_ans2)
-layout_ans1.addLayout(layout_ans3) # разместили столбцы в одной строке
+layout_ans1.addLayout(layout_ans3)
 
 
-RadioGroupBox.setLayout(layout_ans1) # готова "панель" с вариантами ответов 
+RadioGroupBox.setLayout(layout_ans1)
 
 
+# Создаем панель результата
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?') # здесь размещается надпись "правильно" или "неправильно"
+lb_Correct = QLabel('ответ будет тут!') # здесь будет написан текст правильного ответа
+
+
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+
+
+# Размещаем все виджеты в окне:
 layout_line1 = QHBoxLayout() # вопрос
 layout_line2 = QHBoxLayout() # варианты ответов или результат теста
 layout_line3 = QHBoxLayout() # кнопка "Ответить"
 
 
 layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
-layout_line2.addWidget(RadioGroupBox)
+# Размещаем в одной строке обе панели, одна из них будет скрываться, другая показываться:
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
+RadioGroupBox.hide() # эту панель мы уже видели, скроем, посмотрим, как получилась панель с ответом
 
 
 layout_line3.addStretch(1)
@@ -63,6 +81,10 @@ layout_card.addStretch(1)
 layout_card.setSpacing(5) # пробелы между содержимым
 
 
+window = QWidget()
 window.setLayout(layout_card)
+window.setWindowTitle('Memory Card')
 window.show()
+
+
 app.exec()
