df1d7385a2195b514fbd61d9cb85a1eaecd62db4
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-10-15 15:08:35 +0600

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,47 +1,117 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget,  QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QMessageBox, QRadioButton
- 
-def show_win():
-   victory_win = QMessageBox()
-   victory_win.setText('Верно!\nВы выиграли гироскутер')
-   victory_win.exec_()
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, 
+        QGroupBox, QButtonGroup, QRadioButton,  
+        QPushButton, QLabel)
 
 
-def show_lose():
-   victory_win = QMessageBox()
-   victory_win.setText('Нет, в 2015 году\nВы выиграли фирменный плакат')
-   victory_win.exec_()
- 
 app = QApplication([])
-main_win = QWidget()
-main_win.setWindowTitle('Конкурс от Crazy People')
-main_win.resize(400, 200)
- 
-question = QLabel('В каком году канал получил «золотую кнопку» от YouTube?')
-btn_answer1 = QRadioButton('2005')
-btn_answer2 = QRadioButton('2010')
-btn_answer3 = QRadioButton('2015')
-btn_answer4 = QRadioButton('2020')
- 
-layout_main = QVBoxLayout()
-layoutH1 = QHBoxLayout()
-layoutH2 = QHBoxLayout()
-layoutH3 = QHBoxLayout()
-layoutH1.addWidget(question, alignment = Qt.AlignCenter)
-layoutH2.addWidget(btn_answer1, alignment = Qt.AlignCenter)
-layoutH2.addWidget(btn_answer2, alignment = Qt.AlignCenter)
-layoutH3.addWidget(btn_answer3, alignment = Qt.AlignCenter)
-layoutH3.addWidget(btn_answer4, alignment = Qt.AlignCenter)
- 
-layout_main.addLayout(layoutH1)
-layout_main.addLayout(layoutH2)
-layout_main.addLayout(layoutH3)
-main_win.setLayout(layout_main)
- 
-btn_answer3.clicked.connect(show_win)
-btn_answer1.clicked.connect(show_lose)
-btn_answer2.clicked.connect(show_lose)
-btn_answer4.clicked.connect(show_lose)
- 
-main_win.show()
-app.exec_()
+
+
+btn_OK = QPushButton('Ответить') 
+lb_Question = QLabel('Самый сложный вопрос в мире!') 
+
+
+RadioGroupBox = QGroupBox("Варианты ответов")
+
+
+rbtn_1 = QRadioButton('Вариант 1')
+rbtn_2 = QRadioButton('Вариант 2')
+rbtn_3 = QRadioButton('Вариант 3')
+rbtn_4 = QRadioButton('Вариант 4')
+
+
+RadioGroup = QButtonGroup() 
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+
+layout_ans1 = QHBoxLayout()   
+layout_ans2 = QVBoxLayout()
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1) 
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3) 
+layout_ans3.addWidget(rbtn_4)
+
+
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+
+
+RadioGroupBox.setLayout(layout_ans1)
+
+
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?') 
+lb_Correct = QLabel('ответ будет тут!')
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+
+
+layout_line1 = QHBoxLayout() 
+layout_line2 = QHBoxLayout() 
+layout_line3 = QHBoxLayout() 
+
+
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
+AnsGroupBox.hide()
+
+
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_OK, stretch=2) # кнопка должна быть большой
+layout_line3.addStretch(1)
+
+
+layout_card = QVBoxLayout()
+
+
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5) # пробелы между содержимым
+
+
+# ----------------------------------------------------------
+# Виджеты и макеты созданы, далее - функции:
+# ----------------------------------------------------------
+
+
+def show_result():
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Следующий вопрос')
+
+def show_question():
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Ответить')
+    RadioGroup.setExclusive(False) # сняли ограничения, чтобы можно было сбросить выбор радиокнопки
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True) # вернули ограничения, теперь только одна радиокнопка может быть выбрана
+
+def test():
+    if 'Ответить' == btn_OK.text():
+        show_result()
+    else:
+        show_question()
+
+
+window = QWidget()
+window.setLayout(layout_card)
+window.setWindowTitle('Memo Card')
+btn_OK.clicked.connect(test) # проверяем, что панель ответов показывается при нажатии на кнопку
+window.show()
+app.exec()
