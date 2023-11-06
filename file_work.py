449736ea0c9ed5f0eb34171585a724bfc08a6d2f
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-06 15:32:54 +0600

    Update file_work.py

--- file_work.py
+++ file_work.py
@@ -1,72 +1,20 @@
-#напиши здесь свою программу
-with open('quotes.txt', "r", encoding = "UTF-8") as file:
-    for line in file:
-        print(line)
+from turtle import *
+ 
+speed(10)
 
-author = input("Кто написал эти строки? ")
-with open('quotes.txt', "a",  encoding = "UTF-8") as file:
-    file.write( "(" + author + ")" + "\n")
+def kvadrat(storona, cvet):
+    color(cvet)
+    begin_fill()
+    for i in range(4):
+        forward(storona)
+        left(90)
+    end_fill()
 
-while True:
-    answer = input("Хотите добавить ещё одну цитату? (да/нет)")
-    answer = answer.lower()
-    if answer == "да":
-        quote = input("Введите цитату: ")
-        author = input("Введите автора: ")
-        file = open("quotes.txt", "a",  encoding = "UTF-8")
-        file.write(quote+"\n"+"("+author+")"+"\n")
-        file.close()
-    else:
-        break
-
-with open('quotes.txt', "r", encoding = "UTF-8") as file:
-    for line in file:
-        print(line)
+kvadrat(125,'black')
+kvadrat(100,'white')
+kvadrat(75,'gray')
+kvadrat(50,'black')
 
 
-
-
-
-
-
-
-#напиши здесь свою программу
-import time
-
-class Pupil:
-    def __init__(self, Surname, Name, Mark):
-        self.Surname = Surname
-        self.Name = Name
-        self.Mark = Mark
-
-pupils = []
-
-def print_class(pupils):
-    for pupil in pupils:
-        print(pupil.Surname, pupil.Name, "-", pupil.Mark)
-    print("\n")
-
-def print_five(pupils):
-    print("Отличники: ")
-    for pupil in pupils:
-        if pupil.Mark == 5:
-            print(pupil.Surname)
-    print("\n")
-
-def find_average(pupils):
-    average = 0
-    for pupil in pupils:
-        average += pupil.Mark
-    average /= len(pupils)
-    print("Средняя оценка класса:", average)
-
-start_time = time.time()
-with open("pupils_large.txt", "r", encoding = "utf-8") as file:
-    for line in file:
-        data = line.split(" ")
-        pupil = Pupil(data[0], data[1], int(data[2]))
-        pupils.append(pupil)
-
-print_five(pupils)
-find_average(pupils)
-print("Время выполнения: ", (time.time()-start_time), "секунд")
+hideturtle()
+exitonclick()
