ef3497116abb3bf1727005c7dad1b46dcaeae4f6
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-08 15:40:10 +0600

    Update file_work.py

--- file_work.py
+++ file_work.py
@@ -1,50 +1,33 @@
 from turtle import *
- 
-speed(10)
-
-def kvadrat(storona, cvet):
-    color(cvet)
-    begin_fill()
+def fence(color_f):
+    color(color_f)
+    pensize(3)
+    penup()
+    goto(-215, 0)
+    pendown()
     for i in range(4):
-        forward(storona)
         left(90)
-    end_fill()
-
-kvadrat(125,'black')
-kvadrat(100,'white')
-kvadrat(75,'gray')
-kvadrat(50,'black')
-
-
-hideturtle()
-exitonclick()
-
-
-
-
-
-
-
-
-from turtle import*
-pensize(2)
-goto(-50,-50)
-
-def square ():
-    for i in range(4):
-        forward(200)
+        forward(50)
+        right(90)
+        forward(25)
         left(90)
-
-begin_fill()
-square()    
-end_fill() 
-
-penup()
-goto(170,-50)
-pendown()
-begin_fill()
-circle(3)
-end_fill() 
+        forward(50)
+        right(90)
+        forward(25)
+        right(90)
+        forward(50)
+        left(90)
+        forward(25)
+        right(90)
+        forward(50)
+        left(90)
+        forward(25)
+        
+answer = input("Введи корпус: главный корпус/приёмная жителей")
+if answer == "главный корпус":
+    fence("blue")
+if answer == "приёмная жителей":    
+    fence("green")
 hideturtle()
 exitonclick()
 
