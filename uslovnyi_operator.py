e1934376bd05ffebf13133df1d1c47a56d2234f5
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-08 17:06:41 +0600

    Update uslovnyi_operator.py

--- uslovnyi_operator.py
+++ uslovnyi_operator.py
@@ -71,3 +71,44 @@ if answer == "ночь":
 hideturtle()
 exitonclick()
 
+
+
+from turtle import *
+ 
+def speed_ok():
+    color("green")
+    begin_fill()
+    circle(50)
+    end_fill()
+    
+def speed_over():  
+    color("red")
+    penup()
+    goto(0,-70)
+    pendown()
+    begin_fill()
+    circle(18)
+    end_fill()
+ 
+    penup()
+    goto(-10,-10)
+    pendown()
+    begin_fill()
+    forward(20)
+    left(80)
+    forward(100)
+    left(100)
+    forward(55)
+    left(100)
+    forward(100)
+    end_fill()  
+ 
+speed = int(input("Введите скорость транспорта:"))
+if speed <= 60:
+    speed_ok()
+if speed > 60:
+    speed_over()    
+hideturtle()
+exitonclick()
+
+
