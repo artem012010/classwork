188d9dfe806e28b19843074fa2f7757a60791726
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-08 16:58:20 +0600

    Update uslovnyi_operator.py

--- uslovnyi_operator.py
+++ uslovnyi_operator.py
@@ -39,3 +39,35 @@ end_fill()
 hideturtle()
 exitonclick()
 
+
+
+
+from turtle import *
+
+def day():
+    pensize(2)
+    color("yellow")
+    begin_fill()
+    for i in range(18):
+        forward(100)
+        left(100)
+    end_fill()
+    
+def night():
+    pensize(2)
+    color("bisque")
+    begin_fill()
+    circle(50)
+    end_fill()
+    
+answer = input("Какое сейчас время суток (день/ночь)?")
+
+if answer == "день":
+    day()
+
+if answer == "ночь":
+    night()  
+      
+hideturtle()
+exitonclick()
+
