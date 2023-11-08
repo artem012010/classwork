b2f4371d12d292fbed71a091e1ca9f255671d04f
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-08 16:54:46 +0600

    Update uslovnyi_operator.py

--- uslovnyi_operator.py
+++ uslovnyi_operator.py
@@ -1,56 +1,15 @@
 from turtle import *
-from time import *
 
-def light(x,y,col,enabled):
-    if enabled:
-        color(col)
-        penup()
-        goto(x,y)
-        pendown()
-        begin_fill()
-        circle(30)
-        end_fill()
-    else:
-        color(col)
-        penup()
-        goto(x,y)
-        pendown()
-        circle(30)
-
-def ramka():
-    color('black')
-    penup()
-    goto(-50,120)
-    pendown()
-    forward(80)
-    right(90)
-    forward(200)
-    right(90)
-    forward(80)
-    right(90)
-    forward(200)
-    right(90)
-
-while True:
-    ramka()
-    light(0,65,'red',True)
-    light(0,0,'yellow',False)
-    light(0,-65,'green',False)
-    sleep(5)
-    clear()
-    ramka()
-    light(0,65,'red',False)
-    light(0,0,'yellow',True)
-    light(0,-65,'green',False)
-    sleep(5)
-    clear()
-    ramka()
-    light(0,65,'red',False)
-    light(0,0,'yellow',False)
-    light(0,-65,'green',True)
-    sleep(5)
-    clear()
+def star():
+    pensize(2)
+    color('red')
+    begin_fill()
+    for i in range(5):
+        forward(100)
+        left(144)
+    end_fill()
 
 
+star()     
 hideturtle()
 exitonclick()
