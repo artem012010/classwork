215f0113c8094cb9ebb3e91dff8412bc02d1a006
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-11 17:34:34 +0600

    Update panda.py

--- panda.py
+++ panda.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 
 
-# Шаг 1. Загрузка и очистка данных
+# Шаг 1. Загрузка и очистка данных 
 df = pd.read_csv('titanic.csv')
 df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis = 1, inplace = True)
 df[list(pd.get_dummies(df['Embarked']).columns)] = pd.get_dummies(df['Embarked'])
@@ -9,9 +9,10 @@ df['Embarked'].fillna('S', inplace = True)
 df.drop('Embarked', axis = 1, inplace = True)
 
 
-age_1 = df[df['Pclass'] == 1]['Age'].median() 
-age_2 = df[df['Pclass'] == 2]['Age'].median() 
-age_3 = df[df['Pclass'] == 3]['Age'].median() 
+age_1 = df[df['Pclass'] == 1]['Age'].median()
+age_2 = df[df['Pclass'] == 2]['Age'].median()
+age_3 = df[df['Pclass'] == 3]['Age'].median()
+
 
 def fill_age(row):
    if pd.isnull(row['Age']):
@@ -21,6 +22,7 @@ def fill_age(row):
            return age_2
        return age_3
    return row['Age']
+
 
 df['Age'] = df.apply(fill_age, axis = 1)
 
@@ -38,20 +40,18 @@ df['Sex'] = df['Sex'].apply(fill_sex)
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import accuracy_score
 
 X = df.drop('Survived', axis = 1)
 y = df['Survived']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
 
 sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
+X_train = sc.fit_transform(X_train) 
 X_test = sc.transform(X_test)
 
 classifier = KNeighborsClassifier(n_neighbors = 5)
 classifier.fit(X_train, y_train)
 
 y_pred = classifier.predict(X_test)
-print('Процент правильно предсказанных исходов:', accuracy_score(y_test, y_pred) * 100)
-print('Confusion matrix:')
-print(confusion_matrix(y_test, y_pred))
+print('USPEHNIY ISHOD:', accuracy_score(y_test, y_pred) * 100, '%')
