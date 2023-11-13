30c598faca3c5d09c63c0ec1b4733dcaef60dc35
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-13 15:27:12 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,3 +1,4 @@
+#Первый файл является модулем допустим с названием city
 from turtle import *
 
 def background(x,y,wid,hei,col):
@@ -26,3 +27,33 @@ def sun():
         forward(75)
         left(100)
     end_fill()
+
+
+def moon():
+    penup()
+    goto(200,100)
+    pendown()
+    color('white')
+    begin_fill()
+    circle(50)
+    end_fill()
+
+#Второй файл основная часть программы название любое допустим main
+from turtle import *
+from city import *
+
+speed(10)
+
+time = input('Введите время суток')
+if time == 'день':
+    background(-250,-100,500,100,'lightgreen')
+    background(-250,400,500,500,'lightblue')
+    sun()
+if time == 'ночь':
+    background(-250,-100,500,100,'darkgreen')
+    background(-250,400,500,500,'darkblue')
+    moon()
+
+
+hideturtle()
+exitonclick()
