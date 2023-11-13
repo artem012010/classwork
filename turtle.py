4ffc7bd8035d09ec740d11e2af9fe5e719f2561c
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-13 17:05:30 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,59 +1,38 @@
-#Первый файл является модулем допустим с названием city
 from turtle import *
 
-def background(x,y,wid,hei,col):
-    color(col)
+def ground():
+    color('lightgreen')
     penup()
-    goto(x,y)
+    goto(-250,-100)
     pendown()
     begin_fill()
-    forward(wid)
-    right(90)
-    forward(hei)
-    right(90)
-    forward(wid)
-    right(90)
-    forward(hei)
-    right(90)
+    for i in range(2):
+        forward(500)
+        right(90)
+        forward(100)
+        right(90)
+    end_fill()
+
+def sky():
+    color('lightblue')
+    penup()
+    goto(-250,200)
+    pendown()
+    begin_fill()
+    for i in range(2):
+        forward(500)
+        right(90)
+        forward(300)
+        right(90)
     end_fill()
 
 def sun():
-    penup()
-    goto(170,100)
-    pendown()
     color('yellow')
+    penup()
+    goto(180,130)
+    pendown()
     begin_fill()
     for i in range(18):
-        forward(75)
+        forward(70)
         left(100)
     end_fill()
-
-
-def moon():
-    penup()
-    goto(200,100)
-    pendown()
-    color('white')
-    begin_fill()
-    circle(50)
-    end_fill()
-
-#Второй файл основная часть программы название любое допустим main
-from turtle import *
-from city import *
-
-speed(10)
-
-time = input('Введите время суток')
-if time == 'день':
-    background(-250,-100,500,100,'lightgreen')
-    background(-250,400,500,500,'lightblue')
-    sun()
-if time == 'ночь':
-    background(-250,-100,500,100,'darkgreen')
-    background(-250,400,500,500,'darkblue')
-    moon()
-
-
-hideturtle()
-exitonclick()
