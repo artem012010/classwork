f3a212e04f73c408968ebdd61b6455f16ba42a79
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-15 17:46:30 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -18,3 +18,46 @@ for i in range(60):
     rafaelo.left(6)
     leonardo.forward(6)
     leonardo.left(6)
+
+
+
+
+
+
+
+
+from turtle import *
+from random import *
+
+adolf = Turtle()
+iosif = Turtle()
+
+def start_race(x,y,col,name):
+    name.shape('turtle')
+    name.color(col)
+    name.penup()
+    name.goto(x,y)
+
+def dance(name):
+    name.goto(0,0)
+    name.pendown()
+    for i in range(10):
+        name.circle(30)
+        name.left(36)
+
+start_race(-200,30,'black',adolf)
+start_race(-200,-30,'red',iosif)
+
+while adolf.xcor() < 200 and iosif.xcor() < 200:
+    adolf.forward( randint(1,10) )
+    iosif.forward( randint(1,10) )
+
+adolf_result = adolf.xcor()
+iosif_result = iosif.xcor()
+
+winner = max(adolf_result,iosif_result) 
+
+if winner == adolf_result:
+    dance(adolf) 
+if winner == iosif_result:
+    dance(iosif)
