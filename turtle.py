0998c7c9cfae4c7f6cc58026677f55b43c0f743f
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-15 17:10:50 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,59 +1,20 @@
 from turtle import *
 
-adolf = Turtle()
-adolf.color('black')
-
-iosif = Turtle()
-iosif.color('red')
-iosif.left(90)
-
-naruto = Turtle()
-naruto.color('orange')
-naruto.left(180)
-
-donatello = Turtle()
-donatello.color('violet')
-donatello.left(270)
-
-for i in range(120):
-    adolf.forward(3)
-    iosif.forward(3)
-    naruto.forward(3)
-    donatello.forward(3)
-    adolf.left(3)
-    iosif.left(3)
-    naruto.left(3)
-    donatello.left(3)
-
-
-
-
-
-from turtle import *
-from random import *
+donatelo = Turtle()
+donatelo.color('violet')
 
 rafaelo = Turtle()
+rafaelo.color('red')
+rafaelo.left(120)
+
 leonardo = Turtle()
+leonardo.color('blue')
+leonardo.left(240)
 
-def start_race(name,col,x,y):
-    name.color(col)
-    name.shape('turtle')
-    name.penup()
-    name.goto(x,y)
-
-def dance(name):
-    name.goto(0,0)
-    name.pendown()
-    name.circle(50)
-
-start_race(rafaelo,'red',-200,40)
-start_race(leonardo,'blue',-200,-40)
-
-while rafaelo.xcor() < 200 and leonardo.xcor() < 200:
-    rafaelo.forward( randint(5,20) )
-    leonardo.forward( randint(5,20) )
-
-if max(rafaelo.xcor(),leonardo.xcor()) == rafaelo.xcor():
-    dance(rafaelo)
-elif max(rafaelo.xcor(),leonardo.xcor()) == leonardo.xcor():
-    dance(leonardo)
+for i in range(60):
+    donatelo.forward(6)
+    donatelo.left(6)
+    rafaelo.forward(6)
+    rafaelo.left(6)
+    leonardo.forward(6)
+    leonardo.left(6)
