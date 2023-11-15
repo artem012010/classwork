7097fb577c8294e039cabc6d9d80a08bbdf5ed32
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-15 15:10:53 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,93 +1,26 @@
-#Главный файл основная часть программы допустим main.py
-from turtle import *
-from 'Имя вашего модуля' import *
-
-speed(10)
-
-time = input('Введите время суток')
-if time == 'день':
-    ground('lightgreen')
-    sky('lightblue')
-    home()
-    sun()
-if time == 'ночь':
-    ground('darkgreen')
-    sky('darkblue')
-    home()
-    moon()
-
-hideturtle()
-exitonclick()
-
-#Второй файл с функциями для отрисовки обьектов допустим city.py
 from turtle import *
 
-def ground(col):
-    color(col)
-    penup()
-    goto(-250,-100)
-    pendown()
-    begin_fill()
-    for i in range(2):
-        forward(500)
-        right(90)
-        forward(100)
-        right(90)
-    end_fill()
+adolf = Turtle()
+adolf.color('black')
 
-def sky(col):
-    color(col)
-    penup()
-    goto(-250,200)
-    pendown()
-    begin_fill()
-    for i in range(2):
-        forward(500)
-        right(90)
-        forward(300)
-        right(90)
-    end_fill()
+iosif = Turtle()
+iosif.color('red')
+iosif.left(90)
 
-def sun():
-    color('yellow')
-    penup()
-    goto(180,130)
-    pendown()
-    begin_fill()
-    for i in range(18):
-        forward(70)
-        left(100)
-    end_fill()
+naruto = Turtle()
+naruto.color('orange')
+naruto.left(180)
 
+donatello = Turtle()
+donatello.color('violet')
+donatello.left(270)
 
-def moon():
-    color('white')
-    penup()
-    goto(180,100)
-    pendown()
-    begin_fill()
-    circle(50)
-    end_fill()
-
-def home():
-    color('orange')
-    penup()
-    goto(-180,-100)
-    pendown()
-    begin_fill()
-    for i in range(4):
-        forward(50)
-        left(90)
-    end_fill()
-
-    left(90)
-    forward(50)
-    right(90)
-    color('red')
-    begin_fill()
-    for i in range(3):
-        forward(50)
-        left(120)
-    end_fill()
-
-
+for i in range(120):
+    adolf.forward(3)
+    iosif.forward(3)
+    naruto.forward(3)
+    donatello.forward(3)
+    adolf.left(3)
+    iosif.left(3)
+    naruto.left(3)
+    donatello.left(3)
