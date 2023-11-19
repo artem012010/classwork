e897f784ee2a3b6ddc19e8152e671802ddd8f54f
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-19 14:48:01 +0600

    Update pypygame.py

--- pypygame.py
+++ pypygame.py
@@ -17,6 +17,18 @@ class GameSprite(sprite.Sprite):
    def reset(self):
        window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
 
 #Игровая сцена:
 win_width = 700
@@ -27,7 +39,7 @@ background = transform.scale(image.load("background.jpg"), (win_width, win_heigh
 
 
 #Персонажи игры:
-player = GameSprite('hero.png', 5, win_height - 80, 4)
+player = Player('hero.png', 5, win_height - 80, 4)
 monster = GameSprite('cyborg.png', win_width - 80, 280, 2)
 final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
 
@@ -49,6 +61,7 @@ while game:
            game = False
   
    window.blit(background,(0, 0))
+   player.update()
    player.reset()
    monster.reset()
 
