d79b2738984c15652922c39586625605f96a8593
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-20 17:24:48 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -7,11 +7,25 @@ oleg.penup()
 oleg.shape('turtle')
 oleg.color('blue')
 oleg.speed(10)
+oleg.point = 0
+
 def rand_move():
     oleg.goto(randint(-200,200),randint(-200,200))
-# Определи функцию-обработчик catch(x, y), которая обработает клик по черепашке 
-# (успешные клики копятся в свойстве t.points)
-# Создай подписку на событие «клик по объекту-черепашке»
-while True:
+
+def poimali(x,y):
+    oleg.write('Вааай',font=('Arial',20,'normal'))
     rand_move()
-    sleep(1.5)
+    oleg.point += 1
+
+def win():
+    oleg.clear()
+    oleg.goto(-100,0)
+    oleg.write('ПОБЕДА', font=('Arial',50,'bold'))
+    oleg.hideturtle()
+
+oleg.onclick(poimali)
+while oleg.point < 3:
+    rand_move()
+    sleep(2)
+
+win()
