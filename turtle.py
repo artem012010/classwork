d4b17e371df4655209c6cdbef246d0ef43869b2e
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-20 15:42:01 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,6 +1,6 @@
 from turtle import *
 from random import randint
-from time import sleep
+from time import *
 
 oleg = Turtle()
 oleg.shape('turtle')
@@ -8,12 +8,42 @@ oleg.color('blue')
 oleg.speed(10)
 oleg.penup()
 
+clock = Turtle()
+clock.penup()
+
+oleg.points = 0
+
 def rand_move():
     oleg.goto(randint(-200,200), randint(-200,200))
-# Определи функцию-обработчик catch(x, y), которая обработает клик по черепашке 
-# (успешные клики копятся в свойстве t.points)
-# Создай подписку на событие «клик по объекту-черепашке»
-while True:
+
+def touch(x,y):
+    oleg.write('Вай',font=('Arial',20,'normal'))
     rand_move()
-    sleep(0.1)
+    oleg.points += 1
+
+def result(text):
+    oleg.clear()
+    oleg.goto(-100,0)
+    oleg.write(text,font=('Arial',50,'bold'))
+    oleg.hideturtle()
+
+def timer():
+    clock.goto(-200,100)
+    t = abs(start - time())
+    vremya = str(round(t,1))
+    clock.clear()
+    clock.hideturtle()
+    clock.write(vremya,font=('Arial',30,'bold'))
+    return t
     
+oleg.onclick(touch)
+start = time()
+while oleg.points < 3 and timer() < 20:
+    rand_move()
+    sleep(0.5)
+    timer()
+
+if timer() >= 20:
+    result('Проигрыш')
+else:
+    result('Победа')
