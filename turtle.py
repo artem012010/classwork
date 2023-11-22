77a512e74545872471fba84dfc4a3a1a9b4b92a7
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-22 17:39:36 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -11,6 +11,28 @@ def move(x,y):
     sergey.goto(x,y)
     sergey.pendown()
 
+def setGreen():
+    sergey.color('green')
+
+def setBlack():
+    sergey.color('black')
+
+def goUp():
+    sergey.goto(sergey.xcor(),sergey.ycor() + 5)
+def goDown():
+    sergey.goto(sergey.xcor(),sergey.ycor() - 5)
+def goRight():
+    sergey.goto(sergey.xcor() + 5,sergey.ycor())
+def goLeft():
+    sergey.goto(sergey.xcor() - 5,sergey.ycor())
+
 sergey.ondrag(draw)
 screen = sergey.getscreen()
+screen.listen()
 screen.onscreenclick(move)
+screen.onkey(setGreen,'g')
+screen.onkey(setBlack,'b')
+screen.onkey(goUp,'Up')
+screen.onkey(goDown,'Down')
+screen.onkey(goRight,'Right')
+screen.onkey(goLeft,'Left')
