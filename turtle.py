8fa34f1e072d37dfb3c920e6eabd9189e1a5750c
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-11-22 15:45:21 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -2,6 +2,7 @@ from turtle import *
 dali = Turtle() 
 dali.shape('circle')
 dali.pensize(5)
+dali.razmer = 0
 
 def draw(x,y):
     dali.goto(x,y)
@@ -29,6 +30,24 @@ def goLeft():
 def goRight():
     dali.goto(dali.xcor() + 5,dali.ycor())
 
+def beginFill():
+    dali.begin_fill()
+
+def endFill():
+    dali.end_fill()
+
+def cClear():
+    dali.clear()
+
+def SizePlus():
+    dali.razmer += 1
+    dali.pensize(dali.razmer)
+
+def SizeMinus():
+    dali.razmer -= 1
+    dali.pensize(dali.razmer)
+
+
 screen = dali.getscreen()
 screen.listen()
 screen.onscreenclick(move)
@@ -38,4 +57,9 @@ screen.onkey(goUp,'w')
 screen.onkey(goDown,'s')
 screen.onkey(goLeft,'a')
 screen.onkey(goRight,'d')
+screen.onkey(beginFill,'q')
+screen.onkey(endFill,'e')
+screen.onkey(cClear,'c')
+screen.onkey(SizePlus,'o')
+screen.onkey(SizeMinus,'i')
 dali.ondrag(draw)
