313c9a9d7207ea741cc10f5ad1f557e414e8559e
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-06 15:06:22 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,61 +1,71 @@
 from turtle import *
+from random import randint
 
-sergey = Turtle()
-sergey.shape('circle')
-sergey.size = 0
 
-def draw(x,y):
-    sergey.goto(x,y)
+tsize = 20
+s_width = 200
+s_height = 180
 
-def move(x,y):
-    sergey.penup()
-    sergey.goto(x,y)
-    sergey.pendown()
 
-def setGreen():
-    sergey.color('green')
+class Sprite(Turtle):
+   def __init__(self, x, y, step=10, shape='circle', color='black'):
+       Turtle.__init__(self)
+       self.penup()
+       self.speed(0)
+       self.goto(x, y)
+       self.color(color)
+       self.shape(shape)
+       self.step = step
+       self.points = 0
 
-def setBlack():
-    sergey.color('black')
 
-def goUp():
-    sergey.goto(sergey.xcor(),sergey.ycor() + 5)
-def goDown():
-    sergey.goto(sergey.xcor(),sergey.ycor() - 5)
-def goRight():
-    sergey.goto(sergey.xcor() + 5,sergey.ycor())
-def goLeft():
-    sergey.goto(sergey.xcor() - 5,sergey.ycor())
+   def move_up(self):
+       self.goto(self.xcor(), self.ycor() + self.step)
+   def move_down(self):
+       self.goto(self.xcor(), self.ycor() - self.step)
+   def move_left(self):
+       self.goto(self.xcor() - self.step, self.ycor())
+   def move_right(self):
+       self.goto(self.xcor() + self.step, self.ycor())
 
-def beginFill():
-    sergey.begin_fill()
 
-def endFill():
-    sergey.end_fill()
+   def is_collide(self, sprite):
+       dist = self.distance(sprite.xcor(), sprite.ycor())
+       if dist < 30:
+           return True
+       else:
+           return False
 
-def cClear():
-    sergey.clear()
+player = Sprite(0, -100, 10, 'circle', 'orange')
+enemy1 = Sprite(-s_width, 0, 15, 'square', 'red')
+enemy2 = Sprite(s_width, 70, 15, 'square', 'red')
+goal = Sprite(0, 120, 20, 'triangle', 'green')
 
-def penMinus():
-    sergey.size -= 1
-    sergey.pensize(sergey.size)
 
-def penPlus():
-    sergey.size += 1
-    sergey.pensize(sergey.size)
+total_score = 0
 
-sergey.ondrag(draw)
-screen = sergey.getscreen()
-screen.listen()
-screen.onscreenclick(move)
-screen.onkey(setGreen,'g')
-screen.onkey(setBlack,'b')
-screen.onkey(goUp,'Up')
-screen.onkey(goDown,'Down')
-screen.onkey(goRight,'Right')
-screen.onkey(goLeft,'Left')
-screen.onkey(beginFill,'q')
-screen.onkey(endFill,'e')
-screen.onkey(cClear,'c')
-screen.onkey(penPlus,'i')
-screen.onkey(penMinus,'o')
+
+scr = player.getscreen()
+
+
+scr.listen()
+
+
+scr.onkey(player.move_up, 'Up')
+scr.onkey(player.move_left, 'Left')
+scr.onkey(player.move_right, 'Right')
+scr.onkey(player.move_down, 'Down')
+
+
+while total_score < 3:
+
+   if player.is_collide(goal):
+       total_score += 1
+       player.goto(0, -100)
+   if player.is_collide(enemy1) or player.is_collide(enemy2):
+       goal.hideturtle()
+       break
+
+if total_score == 3:
+   enemy1.hideturtle()
+   enemy2.hideturtle()
