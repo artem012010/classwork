9a3bcfa503f18c81c67107c0320cc5fdfb0dfd3e
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-06 15:30:58 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -36,10 +36,30 @@ class Sprite(Turtle):
        else:
            return False
 
+
+   def set_move(self, x_start, y_start, x_end, y_end):
+       self.x_start = x_start
+       self.y_start = y_start       
+       self.x_end = x_end
+       self.y_end = y_end
+       self.goto(x_start, y_start)
+       self.setheading(self.towards(x_end, y_end)) #направление
+  
+   def make_step(self):
+       self.forward(self.step) #направление уже есть
+
+
+       if self.distance(self.x_end, self.y_end) < self.step: #если расстояние меньше полушага
+           self.set_move(self.x_end, self.y_end, self.x_start, self.y_start) #меняем направление
+
+
 player = Sprite(0, -100, 10, 'circle', 'orange')
 enemy1 = Sprite(-s_width, 0, 15, 'square', 'red')
+enemy1.set_move(-s_width, 0, s_width, 0)
 enemy2 = Sprite(s_width, 70, 15, 'square', 'red')
+enemy2.set_move(s_width, 70, -s_width, 70)
 goal = Sprite(0, 120, 20, 'triangle', 'green')
+#goal.set_move(-s_width, 120, s_width, 0)  
 
 
 total_score = 0
@@ -58,13 +78,16 @@ scr.onkey(player.move_down, 'Down')
 
 
 while total_score < 3:
-
+   enemy1.make_step()
+   enemy2.make_step()
+   #goal.make_step()
    if player.is_collide(goal):
        total_score += 1
        player.goto(0, -100)
    if player.is_collide(enemy1) or player.is_collide(enemy2):
        goal.hideturtle()
        break
+
 
 if total_score == 3:
    enemy1.hideturtle()
