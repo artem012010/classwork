032a49c404a3a33c685295748f0c339276d377a6
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-07 15:31:19 +0600

    Update AUTOOTVETCHIK.py

--- AUTOOTVETCHIK.py
+++ AUTOOTVETCHIK.py
@@ -1,15 +1,44 @@
+from time import *
+from random import randint
+
 def otvet(zapros):
     zapros = zapros
+
     if zapros.find('трени') != -1:
         raspisanie()
+
+    if zapros.find('оплат') != -1 or zapros.find('деньг') != -1:
+        oplata()
+
+    if zapros.find('игра') != -1:
+        ugaday()
 
 def raspisanie():
     print('Плавание вт\чт 20:00')
     print('Футбол сб\вс 16:00')
 
+def oplata():
+    trenirovka = int(input('Сколько занятий вы посетили'))
+    print('Вы должны оплатить',trenirovka * 500,'сом')
+
+def ugaday():
+    win = randint(1,10)
+    for i in range(3):
+        num = int(input('Введите число'))
+        if num == win:
+            print('Вы победили')
+            break
+        else:
+            print('попробуй еще раз')
+
+
 print('Вас приветствует спортивный центр ЗЕВС')
+start = time() 
 zapros = input('Что бы вы хотели узнать?')
 while zapros != 'выйти':
     otvet(zapros)
     zapros = input('Еще что нибудь (выйти - завершение)?')
 
+end = time() 
+print('Было приятно пообщаться вы пробыли с нами')
+print(int((end - start)/60),'минут')
