488b1caf3d244aba96c74921c864ff8685992946
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-10 17:28:31 +0600

    Create pydb.py

---
+++ pydb.py
@@ -0,0 +1,58 @@
+import sqlite3
+db_name = 'quiz.sqlite'
+conn = None
+cursor = None
+def open():
+    global conn, cursor
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+def close():
+    cursor.close()
+    conn.close()
+def do(query):
+    cursor.execute(query)
+    conn.commit()
+def clear_db():
+    open()
+    query = '''DROP TABLE IF EXISTS quiz_content'''
+    do(query)
+    query = '''DROP TABLE IF EXISTS question'''
+    do(query)
+    query = '''DROP TABLE IF EXISTS quiz'''
+    do(query)
+    close()
+def create():
+    open()
+    cursor.execute('''PRAGMA foreign_keys=on''')
+    do('''CREATE TABLE IF NOT EXISTS quiz (
+           id INTEGER PRIMARY KEY,
+           name VARCHAR)''')
+    do('''CREATE TABLE IF NOT EXISTS question (
+               id INTEGER PRIMARY KEY,
+               question VARCHAR,
+               answer VARCHAR,
+               wrong1 VARCHAR,
+               wrong2 VARCHAR,
+               wrong3 VARCHAR)''')
+    do('''CREATE TABLE IF NOT EXISTS quiz_content (
+               id INTEGER PRIMARY KEY,
+               quiz_id INTEGER,
+               question_id INTEGER,
+               FOREIGN KEY (quiz_id) REFERENCES quiz (id),
+               FOREIGN KEY (question_id) REFERENCES question (id) )''')
+    close()
+
+
+def add_questions():
+    questions = [
+        ('Сколько месяцев в году имеют 28 дней?', 'Все', 'Один', 'Ни одного', 'Два'),
+        ('Каким станет зелёный утёс, если упадёт в Красное море?', 'Мокрым', 'Красным', 'Не изменится', 'Фиолетовым'),
+        ('Какой рукой лучше размешивать чай?', 'Ложкой', 'Правой', 'Левой', 'Любой'),
+        ('Что не имеет длины, глубины, ширины, высоты, а можно измерить?', 'Время', 'Глупость', 'Море', 'Воздух'),
+        ('Когда сетью можно вытянуть воду?', 'Когда вода замерзла', 'Когда нет рыбы', 'Когда уплыла золотая рыбка', 'Когда сеть порвалась'),
+        ('Что больше слона и ничего не весит?', 'Тень слона', 'Воздушный шар', 'Парашют', 'Облако')
+    ]
+    open()
+    cursor.executemany('''INSERT INTO question (question, answer, wrong1, wrong2, wrong3) VALUES (?,?,?,?,?)''', questions)
+    conn.commit()
+    close()
