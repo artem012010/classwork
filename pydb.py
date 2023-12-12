82688f78d73b44e05aae90d6807e39bdd05fb1c0
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-12 17:23:28 +0600

    Update pydb.py

--- pydb.py
+++ pydb.py
@@ -21,3 +21,23 @@ def clear_db():
     query = '''DROP TABLE IF EXISTS quiz'''
     do(query)
     close()
+def create():
+    open()
+    cursor.execute('''PRAGMA foreign_keys=on''')
+    do('''CREATE TABLE IF NOT EXISTS quiz (
+           id INTEGER PRIMARY KEY,
+           name VARCHAR)''')
+    do('''CREATE TABLE IF NOT EXISTS question (
+               id INTEGER PRIMARY KEY,
+               question VARCHAR,
+               answer VARCHAR,
+               wrong1 VARCHAR,
+               wrong2 VARCHAR,
+               wrong3 VARCHAR)''')
+    do('''CREATE TABLE IF NOT EXISTS quiz_content (
+               id INTEGER PRIMARY KEY,
+               quiz_id INTEGER,
+               question_id INTEGER,
+               FOREIGN KEY (quiz_id) REFERENCES quiz (id),
+               FOREIGN KEY (question_id) REFERENCES question (id) )''')
+    close()
