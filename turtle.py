12e038c163fd9a7077906b06db159f022c18ff23
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-12 14:58:52 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,81 +1,33 @@
 from turtle import *
+pensize(2)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+exitonclick()
 
-# Игровой класс для создания спрайтов
-class Sprite(Turtle):
-    def __init__(self,x,y,step,shape,color):
-        super().__init__()
-        self.penup()
-        self.shape(shape)
-        self.color(color)
-        self.goto(x,y)
-        self.step = step
 
-    #Функции обработчики движений
-    def move_up(self):
-        self.goto(self.xcor(),self.ycor() + self.step)
-    def move_down(self):
-        self.goto(self.xcor(),self.ycor() - self.step)
-    def move_right(self):
-        self.goto(self.xcor() + self.step,self.ycor())
-    def move_left(self):
-        self.goto(self.xcor() - self.step,self.ycor())
+from turtle import *
 
-    #Проверка столкновений
-    def is_collide(self,sprite):
-        dist = self.distance(sprite.xcor(),sprite.ycor())
-        if dist < 30:
-            return True
-        else:
-            return False
+pensize(10)
+color("blue")
+forward(100)
 
-    #Правила движения
-    def move_set(self,start_x,start_y,end_x,end_y):
-        self.start_x = start_x
-        self.start_y = start_y
-        self.end_x = end_x
-        self.end_y = end_y
-        self.goto(start_x,start_y)
-        self.setheading(self.towards(end_x,end_y))
+exitonclick() 
 
-    #Движение врагов
-    def make_step(self):
-        self.forward(self.step)
-        if self.distance(self.end_x,self.end_y) < self.step:
-            self.move_set(self.end_x,self.end_y,self.start_x,self.start_y)
+from turtle import *
 
-# Создаем персонажей
-player = Sprite(0,-100,10,'circle','black')
-gold = Sprite(0,80,0,'triangle','gold')
-enemy1 = Sprite(-50,30,5,'turtle','red')
-enemy2 = Sprite(50,-30,5,'turtle','red')
-enemy1.move_set(-100,30,100,30)
-enemy2.move_set(100,-30,-100,-30)
+pensize(15)
+color('lightblue')
 
-screen = player.getscreen() # Создаем обьект экрана
-screen.listen() # Просим экран слушать клавиши
-#Подписки на события клавиатуры
-screen.onkey(player.move_up,'Up') 
-screen.onkey(player.move_down,'Down') 
-screen.onkey(player.move_right,'Right') 
-screen.onkey(player.move_left,'Left') 
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
 
-point = 0
+exitonclick()
 
-while point < 3:
-
-    enemy1.make_step()
-    enemy2.make_step()
-
-    if player.is_collide(gold):
-        point += 1
-        player.goto(0,-100)
-
-    if player.is_collide(enemy1) or player.is_collide(enemy2):
-        gold.hideturtle()
-        player.goto(-50,-20)
-        player.write('Вы проиграли',font=('Arial',30,'bold'))
-        break
-
-if point == 3:
-    player.goto(-50,-20)
-    player.write('Вы победили',font=('Arial',30,'bold'))
