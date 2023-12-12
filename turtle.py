22baa58eec641afb40364861e3fa606889a96c5d
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-12 15:01:58 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -31,3 +31,22 @@ forward(100)
 
 exitonclick()
 
+
+from turtle import *
+
+pensize(10)
+
+color('pink')
+circle(50)
+
+color('orange')
+circle(50)
+
+color('yellow')
+circle(50)
+
+color('lime')
+circle(50)
+
+exitonclick()
+
