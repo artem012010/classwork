e212f6c26ef4dbe4b411634b41ddf53841484178
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-18 14:55:04 +0600

    Update arkanoid.py

--- arkanoid.py
+++ arkanoid.py
@@ -1,1 +1,13 @@
+import pygame
+pygame.init()
+
+background = (200, 255, 255)
+window = pygame.display.set_mode((500, 500))
+window.fill(background)
+
+clock = pygame.time.Clock()
+
+while True:
+    pygame.display.update()
+    clock.tick(40)
 
