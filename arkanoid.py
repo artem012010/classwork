30ddc7b6cbbf56981b348fbf105a269bd4bcab8d
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-18 15:06:47 +0600

    Update arkanoid.py

--- arkanoid.py
+++ arkanoid.py
@@ -5,9 +5,30 @@ background = (200, 255, 255)
 window = pygame.display.set_mode((500, 500))
 window.fill(background)
 
+class Picture():
+    def __init__(self,filename=None, x=0, y=0, width=10, height=10, color=None):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.fill_color = background
+        self.image = pygame.image.load(filename)
+
+    def fill(self):
+        pygame.draw.rect(window, self.fill_color, self.rect)
+
+    def colliderect(self, rect):
+        return self.rect.colliderect(rect) 
+
+    def draw(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+ball = Picture('ball.png', 160, 200, 50, 50)
+platform = Picture('platform.png', 200, 300, 100, 30)
+
 clock = pygame.time.Clock()
 
 while True:
+    ball.fill()
+    platform.fill()
+    platform.draw()
+    ball.draw()
     pygame.display.update()
     clock.tick(40)
-
