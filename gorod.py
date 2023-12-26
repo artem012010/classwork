30efaffcdac9303973a28283f72b2803ac2bcf8d
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-26 15:13:54 +0600

    Update gorod.py

--- gorod.py
+++ gorod.py
@@ -1,7 +1,8 @@
 from turtle import *
+from random import *
 
-def zemlya():
-    color('lightgreen')
+def zemlya(col):
+    color(col)
     penup()
     goto(-250,-150)
     pendown()
@@ -15,8 +16,8 @@ def zemlya():
     forward(100)
     end_fill()
 
-def nebo():
-    color('lightblue')
+def nebo(col):
+    color(col)
     penup()
     goto(-250,-150)
     pendown()
@@ -26,6 +27,33 @@ def nebo():
         right(90)  
     end_fill()
 
-zemlya()
-nebo()
+def sun(x,y,col,size):
+    penup()
+    goto(x,y)
+    pendown()
+    color(col)
+    begin_fill()
+    for i in range(18):
+        forward(size)
+        left(100)
+    end_fill()
+
+zapros = input('Введите время суток')
+
+if zapros == 'день':
+    zemlya('lightgreen')
+    nebo('lightblue')
+    sun(200,200,'yellow',100)
+
+if zapros == 'ночь':
+    zemlya('darkgreen')
+    nebo('darkblue')
+    for i in range(5):
+        x = randint(-200,200)
+        y = randint(200,350)
+        sun(x,y,'white',20)
+
+
+
+hideturtle()
 exitonclick()
