95fad33024a4da98205efe7e29648ea51da7d42b
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-26 14:53:58 +0600

    Update gorod.py

--- gorod.py
+++ gorod.py
@@ -15,5 +15,17 @@ def zemlya():
     forward(100)
     end_fill()
 
+def nebo():
+    color('lightblue')
+    penup()
+    goto(-250,-150)
+    pendown()
+    begin_fill()
+    for i in range(4):
+        forward(500)
+        right(90)  
+    end_fill()
+
 zemlya()
+nebo()
 exitonclick()
