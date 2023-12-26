6d18bc754c89bbce3c47288a6b0e67ab7987bd21
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-12-26 14:49:38 +0600

    Create gorod.py

---
+++ gorod.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+def zemlya():
+    color('lightgreen')
+    penup()
+    goto(-250,-150)
+    pendown()
+    begin_fill()
+    forward(500)
+    right(90)
+    forward(100)
+    right(90)
+    forward(500)
+    right(90)
+    forward(100)
+    end_fill()
+
+zemlya()
+exitonclick()
