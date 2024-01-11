c451740033370f29746c69a325798832d590fd0a
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-01-11 12:20:44 +0300

    Update game.py

--- game.py
+++ game.py
@@ -6,7 +6,22 @@ adolf = Turtle()
 adolf.color('blue')
 adolf.shape('turtle')
 adolf.penup()
+adolf.points = 0
 
-while True:
+def poimali(x,y):
+    adolf.write('ВАЙ?',font=('Arial',20,'normal'))
+    adolf.points += 1
+    adolf.goto(randint(-200,200),randint(-200,200))
+
+adolf.onclick(poimali)
+
+while adolf.points < 3:
     adolf.goto(randint(-200,200),randint(-200,200))
     sleep(1)
+
+adolf.clear()
+adolf.goto(-100,-50)
+adolf.write('Ты победил',font=('Arial',30,'bold'))
+adolf.hideturtle()
+
+
