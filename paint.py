195352d705fa57c3de3f659808902cbb017c82aa
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-01-16 15:21:24 +0600

    Update paint.py

--- paint.py
+++ paint.py
@@ -13,7 +13,18 @@ def click_screen(x,y):
     gena.goto(x,y)
     gena.pendown()
 
+#функция для обработки клавиши g
+def green_color():
+    gena.color('green')
+
+#функция для обработки клавиши Стрелка вверх
+def moveUp():
+    gena.goto(gena.xcor(),gena.ycor()+5)
+
 screen = gena.getscreen() # Создаем обьект экрана
+screen.listen() # Просим экран слушать клавиатуру
+screen.onkey(green_color,'g') # Подписка на клавишу g
+screen.onkey(moveUp,'Up') # Подписка на клавишу Стрелка
 #Подписка на клик по экрану
 screen.onscreenclick(click_screen)
 #Подписка на перетаскивание черепашки
