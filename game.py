7f4b134b1ddb2f0c62cd0ebde265b81485c4d578
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-01-23 15:17:11 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,86 +1,34 @@
-from turtle import *
-from time import *
-from random import *
+from time import sleep
 
-adolf = Turtle()
-adolf.color('blue')
-adolf.shape('turtle')
-adolf.penup()
-adolf.points = 0
+class Hero():
+    def __init__(self,name,health,power,weapon):
+        self.name = name
+        self.health = health
+        self.power = power
+        self.weapon = weapon
 
-def poimali(x,y):
-    adolf.write('ВАЙ?',font=('Arial',20,'normal'))
-    adolf.points += 1
-    adolf.goto(randint(-200,200),randint(-200,200))
+    def story(self):
+        print('Меня зовут', self.name)
+        print('Уровень моего здоровья', self.health)
+        print('Я сражаюсь', self.weapon,'и наношу',self.power,'урона')
 
-adolf.onclick(poimali)
+    def attack(self,enemy):
+        print(self.name,'наносит удар',enemy.name)
+        print(enemy.name,'качнулся и потерял',self.power,'здоровья')
+        print('Теперь уровень его жизней состовляет', enemy.health - self.power,'хп \n')
+        enemy.health = enemy.health - self.power
 
-while adolf.points < 3:
-    adolf.goto(randint(-200,200),randint(-200,200))
-    sleep(1)
+    def fight(self,enemy):
+        while self.health > 0 and enemy.health > 0:
+            self.attack(enemy)
+            sleep(2)
+            enemy.attack(self)
+            sleep(2)
+        if enemy.health <= 0:
+            print(enemy.name,'Пал в этом сложном бою')
+        else:
+            print(self.name,'Пал в этом сложном бою')
 
-adolf.clear()
-adolf.goto(-100,-50)
-adolf.write('Ты победил',font=('Arial',30,'bold'))
-adolf.hideturtle()
-
-
-
-
-# ВТОРАЯ ВЕРСИЯ
-from turtle import *
-from time import *
-from random import *
-
-adolf = Turtle()
-iosif = Turtle()
-vladimir = Turtle()
-
-def start(name,col,angle):
-    name.color(col)
-    name.shape('turtle')
-    name.penup()
-    name.left(angle)
-
-start(adolf,'black',0)
-start(iosif,'yellow',120)
-start(vladimir,'red',240)
-
-def poimali_gitlera(x,y):
-    adolf.goto(randint(-30,30),randint(-30,30))
-    adolf.left(randint(0,360))
-
-def poimali_stalina(x,y):
-    iosif.goto(randint(-30,30),randint(-30,30))
-    iosif.left(randint(0,360))
-
-def poimali_lenina(x,y):
-    vladimir.goto(randint(-30,30),randint(-30,30))
-    vladimir.left(randint(0,360))
-
-
-def finish(adolf,iosif,vladimir):
-    adolf_out = abs(adolf.xcor()) > 200 or abs(adolf.ycor()) > 200 
-    iosif_out = abs(iosif.xcor()) > 200 or abs(iosif.ycor()) > 200 
-    vladimir_out = abs(vladimir.xcor()) > 200 or abs(vladimir.ycor()) > 200 
-    itog = adolf_out or iosif_out or vladimir_out 
-    return itog 
-
-adolf.onclick(poimali_gitlera)
-iosif.onclick(poimali_stalina)
-vladimir.onclick(poimali_lenina)
-
-start = time()
-while finish(adolf,iosif,vladimir) == False:
-    adolf.forward(5)
-    iosif.forward(5)
-    vladimir.forward(5)
-end = time()
-
-adolf.goto(-150,-50)
-adolf.write('Ты продержался ' + str(int(end-start)) + ' с',font=('Arial',25,'bold'))
-adolf.hideturtle()
-iosif.hideturtle()
-vladimir.hideturtle()
-
-
+hero = Hero('Леонид',100,20,'меч')
+dragon = Hero('Горыныч',200,50,'огонь')
+hero.fight(dragon)
