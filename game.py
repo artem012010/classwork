629b8a2a4607ab355bc7e5691d888d83bbbcef44
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-01-23 15:56:08 +0600

    Update game.py

--- game.py
+++ game.py
@@ -2,11 +2,12 @@ from time import sleep
 from random import randint
 
 class Hero():
-    def __init__(self,name,health,power,weapon):
+    def __init__(self,name,health,power,weapon,armor):
         self.name = name
         self.health = health
         self.power = power
         self.weapon = weapon
+        self.armor = armor
 
     def story(self):
         print('Меня зовут', self.name)
@@ -14,9 +15,13 @@ class Hero():
         print('Я сражаюсь', self.weapon,'и наношу',self.power,'урона')
 
     def attack(self,enemy):
-        enemy.health = enemy.health - self.power * (randint(10,20)*0.1)
-        print(self.name,'наносит удар',enemy.name)
-        print('Теперь уровень его жизней состовляет', enemy.health,'хп \n')
+        crit = randint(10,20)*0.1
+        if enemy.armor > self.power * crit:
+            enemy.health = enemy.health - self.power * crit
+            print(self.name,'наносит удар',enemy.name)
+            print('Теперь уровень его жизней состовляет', enemy.health,'хп \n')
+        else:
+            print('Нет пробития')
 
 
     def fight(self,enemy):
@@ -32,9 +37,9 @@ class Hero():
                 break
             sleep(2)
 
-hero = Hero('Леонид',100,30,'Меч')
-rogue = Hero('Адольф',100,30,'Усы')
-dragon = Hero('Горыныч',150,50,'Огонь')
+hero = Hero('Леонид',100,30,'Меч',40)
+rogue = Hero('Адольф',100,30,'Усы',40)
+dragon = Hero('Горыныч',200,30,'Огонь',40)
 
 print('Квест рыцарь и дракон \n')
 sleep(2)
