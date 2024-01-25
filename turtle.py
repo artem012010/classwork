fd94070bd326ec8737bd92a288d03ea31dab0037
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-01-25 15:19:26 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,31 +1,14 @@
 from turtle import *
-from random import *
 
-def dance(name):
-    name.goto(0,0)
-    name.pendown()
-    for i in range(5):
-        name.forward(100)
-        name.left(144)
+class Sprite(Turtle):
+    def __init__(self,x,y,col,shp):
+        super().__init__()
+        self.penup()
+        self.goto(x,y)
+        self.color(col)
+        self.shape(shp)
 
-def start_race(name,col,x,y):
-    name.penup()
-    name.shape('turtle')
-    name.color(col)
-    name.goto(x,y)
-
-naruto = Turtle()
-start_race(naruto,'orange',-200,50)
-
-saske = Turtle()
-start_race(saske,'blue',-200,-50)
-
-while naruto.xcor() < 200 and saske.xcor() < 200 :
-    naruto.forward( randint(10,20) )
-    saske.forward( randint(10,20) )
-
-winner = max(naruto.xcor(), saske.xcor())
-if naruto.xcor() == winner:
-    dance(naruto)
-if saske.xcor() == winner:
-    dance(saske)
+player = Sprite(0,-100,'black','turtle')
+enemy1 = Sprite(-100,-50,'red','circle')
+enemy2 = Sprite(100,50,'red','circle')
+goal = Sprite(0,100,'green','triangle')
