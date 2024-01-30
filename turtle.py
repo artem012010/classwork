1bd614aa035f2a550eedc6d9a55312337b9e4202
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-01-30 14:59:28 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -9,6 +9,13 @@ class Sprite(Turtle):
         self.color(col)
         self.shape(shp)
 
+    def collide(self,sprite):
+        dist = self.distance(sprite.xcor(),sprite.ycor())
+        if dist < 30:
+            return True
+        else:
+            return False
+
     def move_up(self):
         self.goto(self.xcor(),self.ycor()+5)
     def move_down(self):
@@ -18,7 +25,7 @@ class Sprite(Turtle):
     def move_left(self):
         self.goto(self.xcor()-5,self.ycor())
 
-player = Sprite(0,-100,'black','turtle')
+player = Sprite(0,-200,'black','turtle')
 enemy1 = Sprite(-100,-50,'red','circle')
 enemy2 = Sprite(100,50,'red','circle')
 goal = Sprite(0,100,'green','triangle')
@@ -30,3 +37,17 @@ screen.onkey(player.move_up,'w')
 screen.onkey(player.move_down,'s')
 screen.onkey(player.move_right,'d')
 screen.onkey(player.move_left,'a')
+
+points = 0
+
+while points < 3:
+    if player.collide(goal):
+        player.goto(0,-200)
+        points += 1
+    if player.collide(enemy1) or player.collide(enemy2):
+        goal.hideturtle()
+        break
+
+if points == 3:
+    enemy1.hideturtle()
+    enemy2.hideturtle()
