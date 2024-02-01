7644fb4edf9ca3c6aa8af74154ed51644ad8ed95
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-02-01 15:01:38 +0600

    Update pypygame.py

--- pypygame.py
+++ pypygame.py
@@ -1,27 +1,12 @@
-from turtle import *
+#Подключение нужных модулей
+import pygame
+pygame.init()
+#создание окна игры
+clock = pygame.time.Clock()
+background_color = (255, 255, 0) #цвет фона 
+window = pygame.display.set_mode((500, 500)) #окно программы 
+window.fill(background_color)
 
-class Sprite(Turtle):
-    def __init__(self,x,y,step,shape,color):
-        super().__init__()
-        self.penup()
-        self.shape(shape)
-        self.color(color)
-        self.goto(x,y)
-        self.step = step
-
-    def move_up(self):
-        self.goto(self.xcor(),self.ycor() + self.step )
-    def move_down(self):
-        self.goto(self.xcor(),self.ycor() - self.step )
-    def move_left(self):
-        self.goto(self.xcor() - self.step,self.ycor())
-    def move_right(self):
-        self.goto(self.xcor() + self.step,self.ycor())
-
-screen = pirat.getscreen()
-screen.listen()
-
-screen.onkey(pirat.move_up,'Up')
-screen.onkey(pirat.move_down,'Down')
-screen.onkey(pirat.move_left,'Left')
-screen.onkey(pirat.move_right,'Right')
+while True:
+    pygame.display.update()
+    clock.tick(40)           
