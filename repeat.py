870dcf8c01b51b922f361f56e12f8abc515ff70e
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-02-03 15:35:15 +0600

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -44,3 +44,20 @@ for year in range(start, end+1):
     else:
         print(year, "год - международный конкурс 'Стартап года'.")
 
+
+
+
+months = 'январь февраль март апрель май июнь июль август сентябрь октябрь ноябрь декабрь'.split()
+sales = input('Введите данные о доходах с января по декабрь').split()
+sales = list(map(int, sales))
+growth = []
+fall = []
+for i in range(1, len(sales)):
+    if sales[i] > sales[i-1]:
+        growth.append(months[i])
+    if sales[i] < sales[i-1]:
+        fall.append(months[i])
+print('Месяцы роста доходов:', growth)
+print('Месяцы снижения доходов:', fall)
+
+
