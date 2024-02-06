6faeb50f1a4959a786416a993b834112f0da545b
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-02-06 15:42:07 +0600

    Update arkanoid.py

--- arkanoid.py
+++ arkanoid.py
@@ -6,7 +6,51 @@ window = pygame.display.set_mode((500, 500))
 window.fill(background)
 clock = pygame.time.Clock()
 
+game = True
 
-while True:
+class Sprite():
+    def __init__(self, filename, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.image.load(filename)
+
+    def fill(self):
+        pygame.draw.rect(window, background, self.rect)
+    
+    def draw(self):
+        pygame.draw.rect(window, background, self.rect)  
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+    def colliderect(self, rect):
+        return self.rect.colliderect(rect)
+    
+ball = Sprite('ball.png', 160, 200, 50, 50)
+platform = Sprite('platform.png', 200, 300, 100, 30)
+
+monsters = list()
+count = 9
+for j in range(3):
+  y = 5 + (55 * j)
+  x = 5 + (27.5 * j)
+  for i in range(count):
+      monster = Sprite('enemy.png',x, y, 50, 50)
+      monsters.append(monster)
+      x = x + 55
+  count = count - 1
+
+while game:
+
+    ball.fill()
+    platform.fill()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    for monster in monsters:
+        monster.draw()
+
+    platform.draw()
+    ball.draw()
+
     pygame.display.update()
     clock.tick(40)
