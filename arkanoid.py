4df29fbd95cfa10f227654ff7fa80b8be4696666
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-02-08 15:34:48 +0600

    Update arkanoid.py

--- arkanoid.py
+++ arkanoid.py
@@ -9,6 +9,8 @@ clock = pygame.time.Clock()
 
 move_right = False
 move_left = False
+speed_x = 3
+speed_y = 3
 game = True
 
 
@@ -34,7 +36,7 @@ platform = Sprite('platform.png', 200, 300, 100, 30)
 
 monsters = list()
 count = 9
-for j in range(3):
+for j in range(1):
   y = 5 + (55 * j)
   x = 5 + (27.5 * j)
   for i in range(count):
@@ -47,7 +49,6 @@ while game:
 
     ball.fill()
     platform.fill()
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -68,9 +69,27 @@ while game:
     if move_left:
         platform.rect.x -= 5
 
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+  
+    if  ball.rect.y < 0:
+        speed_x *= -1
+    if ball.rect.x > 450 or ball.rect.x < 0:
+        speed_x *= -1
+    if ball.rect.colliderect(platform.rect):
+        speed_y *= -1
+    if ball.rect.y > 350:
+        break
 
     for monster in monsters:
         monster.draw()
+        if monster.rect.colliderect(ball.rect):
+            monsters.remove(monster)
+            monster.fill()
+            speed_y *= -1
+    
+    if len(monsters) == 0:
+        break
 
     platform.draw()
     ball.draw()
