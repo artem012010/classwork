5806b683bf5ab6ee6c0a7b9a9e15c55b08fa6757
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-02-10 09:28:47 +0600

    Update function.py

--- function.py
+++ function.py
@@ -1,3 +1,11 @@
+Ответы на задания
+
+Задание 1. «Центр: автоматизация»
+1.1. Правильные варианты ответов:
+
+
+
+
 1.2. Программа для клавиатурного тренажёра:
 def check_result(score):
    if score >= 100:
@@ -178,3 +186,182 @@ for i in range(n):
    st_name = input('Введите имя:')
    st_class = input('Введите класс:')
    print_paper(st_name, st_class)
+
+
+
+
+
+
+
+
+Ответы на задания
+
+Задание 1. «Центр: Вложенность»
+1.1. Правильные варианты ответов:
+
+
+1.2. Текст клавиатурного тренажёра:
+def check(grade):
+   if grade < 4:
+       return 'Допуск запрещён!'
+print(check(3))
+
+1.3. Программа после исправления ошибок:
+def get_result(score):
+   if score >= 85:
+       return '1 место'
+   elif score >=65 and score < 85:
+       return '2 место'
+   elif score >=50 and score < 65:
+       return '3 место'
+   else:
+       return 'Повезёт в другой раз!'
+score = int(input('Введите балл:'))
+print('Ваш результат:', get_result(score))
+
+1.4. Дописанная программа:
+def get_result(score):
+   if score >= 85:
+       return '1 место'
+   elif score >=65 and score < 85:
+       return '2 место'
+   elif score >=50 and score < 65:
+       return '3 место'
+   else:
+       return 'Повезёт в другой раз!'
+
+
+def give_reward(result):
+   if result == '1 место':
+       return 'Поездка в Санкт-Петербург'
+   elif result == '2 место':
+       return 'Сертификат в книжный магазин'
+   elif result == '3 место':
+       return 'Настольная игра'
+   else:
+       return 'Сертификат участника'
+score = int(input('Введите балл:'))
+result = get_result(score)
+reward = give_reward(result)
+print('Ваш результат:', result, '-', reward)
+
+1.5. Написанная программа:
+def amount_five():
+  grade = int(input('Оценка (0-завершить):'))
+  amount_five = 0
+  while grade != 0:
+      if grade == 5:
+          amount_five += 1
+      grade = int(input('Оценка (0-завершить):'))
+  return amount_five
+
+
+def set_discount():
+  amount = amount_five()
+  if amount >= 4 and amount <= 5:
+      return 10
+  elif amount > 5:
+      return 15
+  else:
+      return 0
+
+
+print('Скидка на билеты в театр (%):', set_discount())
+
+
+Доп задание 1.1.
+
+
+Доп задание 1.2
+def final_score(amount):
+   score = 0
+   for i in range(amount):
+       mark = int(input('Введите балл:'))
+       score += mark
+   print('Итоговый счёт:', score)
+   return score
+
+
+def give_reward(subjects):
+   score = final_score(subjects)
+   if score > 80:
+       print('Наградить дипломом.')
+   elif score > 50:
+       print('Наградить похвальной грамотой.')
+   else:
+       print('Выдать грамоту об участии.')
+name = input('Введите имя: (стоп - завершить)')
+
+
+while name != 'стоп':
+   subjects = int(input('Число изученных предметов:'))
+   give_reward(subjects)
+   name = input('Введите имя: (стоп - завершить)')
+
+
+Задание 2. «Центр: вложенность 2»
+
+2.1. Правильный порядок:
+
+
+2.2.  Дописанная программа:
+def calc_bmi(weight, height): #вес в кг, рост в м
+  index = weight / (height * height)
+  return index
+
+
+def print_recomendation(weight, height):
+  index = calc_bmi(weight, height)
+  if index <= 18.5:
+      print('У вас недостаточный вес, пройдите на консультацию в кабинет 301.')
+  elif index > 18.5 and index <= 25:
+      print('Ваш вес в норме, пройдите на 3 этаж для продолжения осмотра.')
+  else:
+      print('У вас избыточный вес, пройдите на консультацию в кабинет 410.')
+
+
+weight = float(input('Введите вес (кг):'))
+height = float(input('Введите рост (м):'))
+print_recomendation(weight, height)
+
+
+
+2.3. Написанная программа:
+def control_rating(rating):
+   if 65 <= rating <= 100:
+       print('Успеваемость в норме.')
+   else:
+       print('Низкая успеваемость!')
+
+
+n = 3
+for i in range(n):
+   rating = int(input('Балл:'))
+   control_rating(rating)
+
+Доп задание. 2.1:
+
+
+
+Доп задание. 2.2:
+def info_lab():
+    print('Лаборатория находится в аудитории A203.')
+    question()
+
+
+def info_lek(): 
+    print('Лекторий находится в аудитории A112.')
+    question()
+
+
+def question():
+    answer = input('Что вас интересует? (стоп - завершить)')
+    if answer == 'лаборатория':
+        info_lab()
+    elif answer == 'лекторий':
+        info_lek()
+    elif answer == 'стоп':
+        print('До свидания!')
+
+
+question()
