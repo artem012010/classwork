5d0db0c0d514d073fe4acc6c947603f380a6db80
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-02-11 15:27:39 +0600

    Update kivy.py

--- kivy.py
+++ kivy.py
@@ -1,19 +1,20 @@
-# расположение виджетов "друг на друге"
+# кнопка и надпись подписываются на события
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 
-class MyApp(App):
-   def build(self):
-      txt = Label(text='Это надпись')
-      btn = Button(text='Это кнопка')
-      layout = BoxLayout()
-      layout.add_widget(txt)
-      layout.add_widget(btn) # эта команда делает видимыми сразу две кнопки (пробуйте убрать)
-      return layout
+def tst():
+    print('KUKU!')
 
-MyApp().run()
-# вторая кнопка визуально не находится на первой!
-# расположением своих потомков управляют только виджеты-макеты (layout)
-# остальные виджеты (например, кнопка) не влияют на место и размер своих потомков
+class MyApp(App):
+    def build(self):
+        txt = Label(text='Это надпись')
+        btn = Button(text='Это кнопка')
+        btn.on_press = tst 
+        layout = BoxLayout()
+        layout.add_widget(txt)
+        layout.add_widget(btn)
+        return layout
+
+MyApp().run() # проверяйте, что будет печататься при кликах по кнопке и по надписи
