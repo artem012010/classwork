c49b43b97fc8395bd50e311a879efcd4c2806e5e
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-02-17 15:26:49 +0600

    Update kivy.py

--- kivy.py
+++ kivy.py
@@ -16,11 +16,46 @@ class ScrButton(Button):
        self.screen.manager.transition.direction = self.direction
        self.screen.manager.current = self.goal
 
+class MainScr(Screen):
+   def __init__(self, **kwargs):
+       super().__init__(**kwargs)
 
+       vl = BoxLayout(orientation='vertical', padding=8, spacing=8)
+       hl = BoxLayout()
+       txt = Label(text= 'Выбери экран')
+ 
+       vl.add_widget(ScrButton(self, direction='down', goal='first', text="1"))
+       vl.add_widget(ScrButton(self, direction='left', goal='second', text="2"))
+       vl.add_widget(ScrButton(self, direction='up', goal='third', text="3"))
+       vl.add_widget(ScrButton(self, direction='right', goal='fourth', text="4"))
+ 
+       hl.add_widget(txt)
+       hl.add_widget(vl)
+       self.add_widget(hl)
 
+class FirstScr(Screen):
+   def __init__(self, **kwargs):
+       super().__init__(**kwargs)
+       btn_back = ScrButton(self, direction='up', goal='main', text="Назад", size_hint=(.5, 1), pos_hint={'right': 1})
+       self.add_widget(btn_back)
 
+class SecondScr(Screen):
+   def __init__(self, **kwargs):
+       super().__init__(**kwargs)
+       btn_back = ScrButton(self, direction='up', goal='main', text="Назад", size_hint=(.5, 1), pos_hint={'right': 1})
+       self.add_widget(btn_back)
 
+class ThirdScr(Screen):
+   def __init__(self, **kwargs):
+       super().__init__(**kwargs)
+       btn_back = ScrButton(self, direction='up', goal='main', text="Назад", size_hint=(.5, 1), pos_hint={'right': 1})
+       self.add_widget(btn_back)
 
+class FourthScr(Screen):
+   def __init__(self, **kwargs):
+       super().__init__(**kwargs)
+       btn_back = ScrButton(self, direction='up', goal='main', text="Назад", size_hint=(.5, 1), pos_hint={'right': 1})
+       self.add_widget(btn_back)
 
 class MyApp(App):
    def build(self):
