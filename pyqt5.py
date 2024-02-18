13a02a3a0f4cefcb7f2048cd73af8a515c749f11
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-02-18 09:56:34 +0600

    Update pyqt5.py

--- pyqt5.py
+++ pyqt5.py
@@ -6,6 +6,23 @@ app = QApplication([])
  
 # главное окно:
 my_win = QWidget()
+my_win.setWindowTitle('Определитель победителя')
+my_win.move(100, 100)
+my_win.resize(400, 200)
+
+
+#виджеты окна: кнопка и надпись
+button = QPushButton('Сгенерировать')
+text = QLabel('Нажми, чтобы узнать победителя')
+winner = QLabel('?')
+
+
+#расположение виджетов
+line = QVBoxLayout()
+line.addWidget(text, alignment = Qt.AlignCenter)
+line.addWidget(winner, alignment = Qt.AlignCenter)
+line.addWidget(button, alignment = Qt.AlignCenter)
+my_win.setLayout(line)
 
 
 
