cd4704059df0aaeef4e93df194a985a758ca8b1a
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-02-18 09:31:18 +0600

    Create pyqt5.py

---
+++ pyqt5.py
@@ -0,0 +1,13 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from random import randint
+ 
+app = QApplication([])
+ 
+# главное окно:
+my_win = QWidget()
+
+
+
+my_win.show()
+app.exec_()
