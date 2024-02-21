e34b2e570fd0c3112d675bbf1139e19b45feda07
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-02-21 15:45:39 +0600

    Update pyqt5.py

--- pyqt5.py
+++ pyqt5.py
@@ -1,8 +1,25 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget,  QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QMessageBox, QRadioButton
 
-app = QApplication([]) # создаем приложение
-main_win = QWidget() # создаем окно
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle('Конкурс от Crazy People')
+main_win.resize(400, 200)
+ 
+question = QLabel('В каком году канал получил «золотую кнопку» от YouTube?')
+btn_answer1 = QRadioButton('2005')
+btn_answer2 = QRadioButton('2010')
+btn_answer3 = QRadioButton('2015')
+btn_answer4 = QRadioButton('2020')
+ 
+layout_main = QVBoxLayout()
+layout_main.addWidget(question, alignment = Qt.AlignCenter)
+layout_main.addWidget(btn_answer1, alignment = Qt.AlignCenter)
+layout_main.addWidget(btn_answer2, alignment = Qt.AlignCenter)
+layout_main.addWidget(btn_answer3, alignment = Qt.AlignCenter)
+layout_main.addWidget(btn_answer4, alignment = Qt.AlignCenter)
+ 
+main_win.setLayout(layout_main)
 
 main_win.show() # Показать окно
 app.exec_() # Закрытие по крестику
