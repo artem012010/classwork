3c7f5155f3474860476dc80fdbe64e8bdcce4341
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-02-25 15:38:05 +0600

    Update panda.py

--- panda.py
+++ panda.py
@@ -1,57 +1,34 @@
-import pandas as pd 
+import pandas as pd
+df = pd.read_csv('GoogleApps.csv')
 
 
-# Шаг 1. Загрузка и очистка данных 
-df = pd.read_csv('titanic.csv')
-df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis = 1, inplace = True)
-df[list(pd.get_dummies(df['Embarked']).columns)] = pd.get_dummies(df['Embarked'])
-df['Embarked'].fillna('S', inplace = True)
-df.drop('Embarked', axis = 1, inplace = True)
+# 1 Сколько всего приложений с категорией ('Category') 'BUSINESS'?
+print(df['Category'].value_counts())
 
 
-age_1 = df[df['Pclass'] == 1]['Age'].median()
-age_2 = df[df['Pclass'] == 2]['Age'].median()
-age_3 = df[df['Pclass'] == 3]['Age'].median()
+# 2 Чему равно соотношение количества приложений для подростков ('Teen') и для детей старше 10 ('Everyone 10+')?
+# Ответ запиши с точностью до сотых.
+temp = df['Content Rating'].value_counts()
+print('Соотношение:', round(temp['Teen'] / temp['Everyone 10+'], 2))
 
 
-def fill_age(row):
-   if pd.isnull(row['Age']):
-       if row['Pclass'] == 1:
-           return age_1
-       if row['Pclass'] == 2:
-           return age_2
-       return age_3
-   return row['Age']
+# 3.1 Чему равен средний рейтинг ('Rating') платных ('Paid') приложений?
+# Ответ запиши с точностью до сотых.
+temp = df.groupby(by = 'Type')['Rating'].mean()
+print(temp['Paid'])
 
+# 3.2 На сколько средний рейтинг ('Rating') бесплатных ('Free') приложений меньше среднего рейтинга платных ('Paid')?
+# Ответ запиши с точностью до сотых.
+print(round(temp['Paid'] - temp['Free'], 2))
 
-df['Age'] = df.apply(fill_age, axis = 1)
+# 4 Чему равен минимальный и максимальный размер ('Size') приложений в категории ('Category') 'COMICS'?
+# Запиши ответы с точностью до сотых.
+print(df.groupby(by = 'Category')['Size'].agg(['min', 'max']))
 
+# Бонус 1. Сколько приложений с рейтингом ('Rating') строго больше 4.5 в категории ('Category') 'FINANCE'?
+temp = df[df['Rating'] > 4.5]['Category'].value_counts()
+print(temp['FINANCE'])
 
-def fill_sex(sex):
-    if sex == 'male':
-        return 1
-    return 0
-
-
-df['Sex'] = df['Sex'].apply(fill_sex)
-
-
-# Шаг 2. Создание модели
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-
-X = df.drop('Survived', axis = 1)
-y = df['Survived']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
-
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train) 
-X_test = sc.transform(X_test)
-
-classifier = KNeighborsClassifier(n_neighbors = 5)
-classifier.fit(X_train, y_train)
-
-y_pred = classifier.predict(X_test)
-print('USPEHNIY ISHOD:', accuracy_score(y_test, y_pred) * 100, '%')
+# Бонус 2. Чему равно соотношение бесплатных ('Free') и платных ('Paid') игр с рейтингом ('Rating') больше 4.9?
+temp = df[(df['Category'] == 'GAME') & (df['Rating'] > 4.9)]['Type'].value_counts()
+print(temp['Free'] / temp['Paid'])
