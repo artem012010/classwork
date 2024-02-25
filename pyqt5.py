65deaba890f233ca3da257d3ad484ae504a34efa
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-02-25 09:56:30 +0600

    Update pyqt5.py

--- pyqt5.py
+++ pyqt5.py
@@ -1,50 +1,62 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget,  QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QMessageBox, QRadioButton
- 
-def show_win():
-   victory_win = QMessageBox()
-   victory_win.setText('Верно!\nВы выиграли гироскутер')
-   victory_win.exec_()
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel)
 
-
-def show_lose():
-   victory_win = QMessageBox()
-   victory_win.setText('Нет, в 2015 году\nВы выиграли фирменный плакат')
-   victory_win.exec_()
- 
 app = QApplication([])
-main_win = QWidget()
-main_win.setWindowTitle('Конкурс от Crazy People')
-main_win.resize(400, 200)
- 
-question = QLabel('В каком году канал получил «золотую кнопку» от YouTube?')
-btn_answer1 = QRadioButton('2005')
-btn_answer2 = QRadioButton('2010')
-btn_answer3 = QRadioButton('2015')
-btn_answer4 = QRadioButton('2020')
- 
-layout_main = QVBoxLayout()
 
-layoutH1 = QHBoxLayout()
-layoutH2 = QHBoxLayout()
-layoutH3 = QHBoxLayout()
+window = QWidget()
+window.setWindowTitle('Memo Card')
 
-layoutH1.addWidget(question, alignment = Qt.AlignCenter)
-layoutH2.addWidget(btn_answer1, alignment = Qt.AlignCenter)
-layoutH2.addWidget(btn_answer2, alignment = Qt.AlignCenter)
-layoutH3.addWidget(btn_answer3, alignment = Qt.AlignCenter)
-layoutH3.addWidget(btn_answer4, alignment = Qt.AlignCenter)
- 
-layout_main.addLayout(layoutH1)
-layout_main.addLayout(layoutH2)
-layout_main.addLayout(layoutH3)
+'''Интерфейс приложения Memory Card'''
+btn_OK = QPushButton('Ответить') # кнопка ответа
+lb_Question = QLabel('В каком году была основана Москва?') # текст вопроса
 
-main_win.setLayout(layout_main)
- 
-btn_answer3.clicked.connect(show_win)
-btn_answer1.clicked.connect(show_lose)
-btn_answer2.clicked.connect(show_lose)
-btn_answer4.clicked.connect(show_lose)
- 
-main_win.show()
-app.exec_()
+
+RadioGroupBox = QGroupBox("Варианты ответов") # группа на экране для переключателей с ответами
+rbtn_1 = QRadioButton('1147')
+rbtn_2 = QRadioButton('1242')
+rbtn_3 = QRadioButton('1861')
+rbtn_4 = QRadioButton('1943')
+
+layout_ans1 = QHBoxLayout()   
+layout_ans2 = QVBoxLayout() # вертикальные будут внутри горизонтального
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1) # два ответа в первый столбец
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3) # два ответа во второй столбец
+layout_ans3.addWidget(rbtn_4)
+
+
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3) # разместили столбцы в одной строке
+
+
+RadioGroupBox.setLayout(layout_ans1) # готова "панель" с вариантами ответов 
+
+
+layout_line1 = QHBoxLayout() # вопрос
+layout_line2 = QHBoxLayout() # варианты ответов или результат теста
+layout_line3 = QHBoxLayout() # кнопка "Ответить"
+
+
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+layout_line2.addWidget(RadioGroupBox)
+
+
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_OK, stretch=2) # кнопка должна быть большой
+layout_line3.addStretch(1)
+
+# Теперь созданные строки разместим друг под другой:
+layout_card = QVBoxLayout()
+
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5) # пробелы между содержимым
+
+
+window.setLayout(layout_card)
+window.show()
+app.exec()
