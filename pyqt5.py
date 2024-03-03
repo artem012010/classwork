e7a647a029d94eb1a33f2edad8e54e5f1db51e4d
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-03 16:57:25 +0600

    Update pyqt5.py

--- pyqt5.py
+++ pyqt5.py
@@ -4,6 +4,15 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QG
 from random import shuffle
 
 
+class Question():
+    def __init__(self, question, right_answer, wrong1, wrong2, wrong3):
+        self.question = question
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+
 app = QApplication([])
 btn_OK = QPushButton('Ответить') 
 lb_Question = QLabel('Самый сложный вопрос в мире!')
