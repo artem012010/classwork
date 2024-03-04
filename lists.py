42fa2c824ab29a4bb17bb20a0e4776c809b377a0
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-04 17:43:10 +0600

    Update lists.py

--- lists.py
+++ lists.py
@@ -10,3 +10,16 @@ while subject != '0':
 
 subjects.sort()
 print('Список предметов:', subjects)
+
+
+marks = list()
+amount = 0
+mark = int(input('Введи оценку (0 - остановить ввод):'))
+while mark != 0:
+   if mark > 2:
+      amount += 1
+   marks.append(mark)
+   mark = int(input('Введи балл (0 - остановить ввод):'))
+progress = (amount)/len(marks)*100
+print('Список оценок:', marks)
+print('Успеваемость:', progress)
