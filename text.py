7cc86ec6cf970bf271208bc5ff91812459101b0d
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-09 10:15:10 +0600

    Update text.py

--- text.py
+++ text.py
@@ -17,3 +17,28 @@ while otvet != 'нет':
 with open('quotes.txt', 'r', encoding='UTF-8') as file:
     data = file.read()
     print(data)
+
+
+
+from time import time
+
+summa_ocenok = 0
+kolvo_uchenikov = 0
+otlichniki = list()
+
+start = time()
+
+with open('pupils.txt', 'r', encoding='UTF-8') as file:
+    for pupil in file:
+        pupil = pupil.split()
+        summa_ocenok += int(pupil[2])
+        kolvo_uchenikov += 1
+        if pupil[2] == '5':
+            otlichniki.append(pupil[0])
+
+end = time()
+
+print('Средний балл:', summa_ocenok/kolvo_uchenikov)
+print('Количество отличников',len(otlichniki))
+print('Анализ занял:',int(end-start),'с')
+        
