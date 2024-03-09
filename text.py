108b621503c11afc35e1e7b0e6d72238ed14ac0f
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-09 09:44:25 +0600

    Update text.py

--- text.py
+++ text.py
@@ -1,73 +1,19 @@
-with open('quotes.txt', "r", encoding = "UTF-8") as file:
-    for line in file:
-        print(line)
-
-author = input("Кто написал эти строки? ")
-with open('quotes.txt', "a",  encoding = "UTF-8") as file:
-    file.write("Автор:"+author+"\n")
-
-while True:
-    answer = input("Хотите добавить ещё одну цитату? (да/нет)").lower()
-    if answer == "да":
-        quote = input("Введите цитату: ")
-        author = input("Введите автора: ")
-        file = open("quotes.txt", "a",  encoding = "UTF-8")
-        file.write(quote+"\n"+"Автор:"+author+"\n")
-        file.close()
-    else:
-        break
-
 with open('quotes.txt', 'r', encoding='UTF-8') as file:
     data = file.read()
     print(data)
 
+author = input('Введите автора цитаты:')
+with open('quotes.txt','a',encoding='UTF-8') as file:
+    file.write('\nАвтор:' + author + '\n')
 
+otvet = input('Хотите добавить еще одну цитату?')
+while otvet != 'нет':
+    with open('quotes.txt','a',encoding='UTF-8') as file:
+        quot = input('Введите цитату:')
+        author = input('Введите автора:')
+        file.write(quot + '\nАвтор:' + author + '\n')
+    otvet = input('Хотите добавить еще одну цитату?')
 
-
-
-
-
-
-
-summa = 0
-kolvo = 0
-otlichniki = []
-
-with open('pupils.txt','r',encoding='UTF-8') as file:
-    for pupil in file:
-        pupil = pupil.split()
-        summa += int(pupil[2])
-        kolvo += 1
-        if pupil[2] == '5':
-            otlichniki.append(pupil[0])
-
-print('Средний балл',summa/kolvo)
-print('Список отличников:',otlichniki)
-
-
-
-
-
-
-
-from time import time
-
-summa = 0
-kolvo = 0
-otlichniki = []
-
-start = time() 
-
-with open('pupils_large.txt','r',encoding='UTF-8') as file:
-    for pupil in file:
-        pupil = pupil.split()
-        summa += int(pupil[2])
-        kolvo += 1
-        if pupil[2] == '5':
-            otlichniki.append(pupil[0])
-
-end = time()
-
-print('Средний балл',summa/kolvo)
-print('Количество отличников:',len(otlichniki))
-print('Прошло',round(end - start,1),'c')
+with open('quotes.txt', 'r', encoding='UTF-8') as file:
+    data = file.read()
+    print(data)
