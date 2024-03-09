62d3b6cc197eafa5e562fa1dbcdf52d7901434e0
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-09 17:16:17 +0600

    Update text.py

--- text.py
+++ text.py
@@ -1,44 +1,12 @@
-with open('quotes.txt', 'r', encoding='UTF-8') as file:
-    data = file.read()
-    print(data)
-
-author = input('Введите автора цитаты:')
-with open('quotes.txt','a',encoding='UTF-8') as file:
-    file.write('\nАвтор:' + author + '\n')
-
-otvet = input('Хотите добавить еще одну цитату?')
+otvet = input('Хотите добавить цитату?')
 while otvet != 'нет':
-    with open('quotes.txt','a',encoding='UTF-8') as file:
-        quot = input('Введите цитату:')
-        author = input('Введите автора:')
-        file.write(quot + '\nАвтор:' + author + '\n')
-    otvet = input('Хотите добавить еще одну цитату?')
+    quot = input('Введите цитату: ')
+    author = input('Введите автора: ')
+    with open('quotes.txt', 'a', encoding='UTF-8') as file:
+        file.write('\n' + quot + '\nАвтор: ' + author )
+    otvet = input('Хотите добавить цитату?')
 
 with open('quotes.txt', 'r', encoding='UTF-8') as file:
     data = file.read()
     print(data)
 
-
-
-from time import time
-
-summa_ocenok = 0
-kolvo_uchenikov = 0
-otlichniki = list()
-
-start = time()
-
-with open('pupils.txt', 'r', encoding='UTF-8') as file:
-    for pupil in file:
-        pupil = pupil.split()
-        summa_ocenok += int(pupil[2])
-        kolvo_uchenikov += 1
-        if pupil[2] == '5':
-            otlichniki.append(pupil[0])
-
-end = time()
-
-print('Средний балл:', summa_ocenok/kolvo_uchenikov)
-print('Количество отличников',len(otlichniki))
-print('Анализ занял:',int(end-start),'с')
-        
