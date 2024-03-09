b68f316dcae1c6c09bdf48c57fdbe5d656997b5d
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-09 17:40:15 +0600

    Update text.py

--- text.py
+++ text.py
@@ -10,3 +10,24 @@ with open('quotes.txt', 'r', encoding='UTF-8') as file:
     data = file.read()
     print(data)
 
+
+from time import time
+summa = 0
+kolvo = 0
+otlichniki = list()
+
+start = time()
+
+with open('pupils.txt', 'r', encoding='UTF-8') as file:
+    for pupil in file:
+        pupil = pupil.split()
+        summa += int(pupil[2])
+        kolvo += 1
+        if pupil[2] == '5':
+            otlichniki.append(pupil[0])
+
+end = time() 
+
+print('Средний балл учеников', summa/kolvo)
+print('Количество отличников:', len(otlichniki))
+print(end - start, 'с')
