895fe417fd205a40e08ae2652cd01bdc0116dbdd
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-10 09:48:46 +0600

    Update zametki.py

--- zametki.py
+++ zametki.py
@@ -1,2 +1,16 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QLabel,QVBoxLayout,QHBoxLayout,QListWidget,QTextEdit,QLineEdit
+
+app = QApplication([]) # Создаем приложение
+'''СОЗДАНИЕ ЭЛЕМЕНТОВ ИНТЕРФЕЙСА'''
+window = QWidget() # Создаем окошко
+window.setWindowTitle('Умные заметки') # Добовляем название окна
+text_field = QTextEdit() # Поле с текстом
+'''РАСПОЛОЖЕНИЕ ВИДЖЕТОВ'''
+main_layout = QVBoxLayout() # Создаем вертикальный макет
+main_layout.addWidget(text_field) # Добовляем в него виджет с текстом
+
+
+window.setLayout(main_layout) # Устанавливаем главную линию к окну
+window.show()
+app.exec()
