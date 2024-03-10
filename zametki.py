3586b83f1f4fd66c55e9d80785cad5eaa848c30d
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-10 09:55:53 +0600

    Update zametki.py

--- zametki.py
+++ zametki.py
@@ -6,11 +6,14 @@ app = QApplication([]) # Создаем приложение
 window = QWidget() # Создаем окошко
 window.setWindowTitle('Умные заметки') # Добовляем название окна
 text_field = QTextEdit() # Поле с текстом
+notes_list = QListWidget() # Список с заметками
+tag_list = QListWidget() # Список с тегами
 '''РАСПОЛОЖЕНИЕ ВИДЖЕТОВ'''
 main_layout = QVBoxLayout() # Создаем вертикальный макет
-main_layout.addWidget(text_field) # Добовляем в него виджет с текстом
-
-
+main_layout.addWidget(text_field) # Добавляем  виджет с текстом
+main_layout.addWidget(notes_list) # Добавляем виджет cписка заметок
+main_layout.addWidget(tag_list) # Добавляем виджет cписка тегов
+'''ЗАПУСК'''
 window.setLayout(main_layout) # Устанавливаем главную линию к окну
 window.show() # Показать окно
 app.exec() # Не закрывать до нажатия крестика
